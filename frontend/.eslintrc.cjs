498b753f4469a02dd05cf1dbea4b592ad45d740f
Mario Gottardo <mario.gottardo27@gmail.com>
2023-07-21 22:35:33 +0200

    React app lint and test configuration

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,18 +1,62 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+    ecmaFeatures: {
+      "jsx": true
+    }
+  },
+  settings: {
+    "react": {
+      "version": "detect"
+    },
+    "import/resolver": {
+      "node": {
+        "paths": [
+          "src"
+        ],
+        "extensions": [
+          ".js",
+          ".jsx",
+          ".ts",
+          ".tsx"
+        ]
+      }
+    }
+  },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react/jsx-runtime',
+    // By extending from a plugin config, we can get recommended rules without having to add them manually.
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
+    // Make sure it's always the last config, so it gets the chance to override other configs.
+    'eslint-config-prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    "import/no-unresolved": [
+      'off', 
+      { "caseSensitive": false }
+    ],
+    "prettier/prettier": "error",
+    "@typescript-eslint/strict-boolean-expressions": "warn"
   },
 }
---
+++ frontend/test/trivialTest.test.ts
@@ -0,0 +1,6 @@
+import { expect, test } from "vitest"
+
+test('the framework is working', () => {
+    const a = 1
+    expect(a).toEqual(1)
+})
