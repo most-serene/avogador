6e1697a02b555ddadda24d84d4d7dbd239a90dfd
Alessio-Campa <ale.campa00@gmail.com>
2023-07-27 22:03:21 +0200

    AVG-5 AVG-6 AVG-7 created Repositories
    Created repositories for User, Course and UserCourse entities

---
+++ backend/services/usercourse/src/main/java/eu/mostserene/avogador/usercourse/courses/CourseRepository.java
@@ -0,0 +1,6 @@
+package eu.mostserene.avogador.usercourse.courses;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
