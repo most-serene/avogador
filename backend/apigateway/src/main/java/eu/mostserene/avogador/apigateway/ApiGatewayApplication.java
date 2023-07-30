9b1f4a5ee999669f0aad443626dc27007cefe214
Mario Gottardo <mario.gottardo27@gmail.com>
2023-07-30 08:45:56 +0200

    AVG-29: userService linked to gateway

--- backend/apigateway/src/main/java/eu/mostserene/avogador/apigateway/ApiGatewayApplication.java
+++ backend/apigateway/src/main/java/eu/mostserene/avogador/apigateway/ApiGatewayApplication.java
@@ -1,10 +1,33 @@
 package eu.mostserene.avogador.apigateway;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController("/")
+@Slf4j
 public class ApiGatewayApplication {
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("geoApi", r -> r.path("/users/**")
+						.filters(f -> f.rewritePath("/users/(?<segment>.*)", "/${segment}"))
+						.uri("http://users:8080"))
+				.build();
+	}
+
+
+	@GetMapping("/get")
+	String foo() {
+		return  "gateway online";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
