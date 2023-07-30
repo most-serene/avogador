b52cf928131acb5fea5514cbddab91c875fc00ed
Mario Gottardo <mario.gottardo27@gmail.com>
2023-07-30 11:52:53 +0200

    AVG-25: google login and axios setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = document.getElementById("root");
 
 if (root) {
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
-      <App />
+      <GoogleOAuthProvider
+        clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID as string}
+      >
+        <App />
+      </GoogleOAuthProvider>
     </React.StrictMode>,
   );
 }
