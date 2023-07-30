c2c0e7321486860614d67ffd4f279e32aa5cad89
Alessio-Campa <ale.campa00@gmail.com>
2023-07-30 14:33:33 +0200

    AVG-40 added Course endpoint
    - added Archive Course endpoint
    - added tests
    - added necessary services methods

--- backend/services/courseservice/src/main/java/eu/mostserene/avogador/courseservice/filesystem/FileSystemService.java
+++ backend/services/courseservice/src/main/java/eu/mostserene/avogador/courseservice/filesystem/FileSystemService.java
@@ -5,4 +5,5 @@ import eu.mostserene.avogador.courseservice.courses.Course;
 public interface FileSystemService {
     Integer createCourse(Long courseId);
     Integer deleteCourse(Long courseId);
+    Integer archiveCourse(Long courseId);
 }
