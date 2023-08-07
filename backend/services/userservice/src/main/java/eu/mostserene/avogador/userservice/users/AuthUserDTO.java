8c6d44704a7d3077d99b3028da72f1d48c134460
Mario Gottardo <mario.gottardo27@gmail.com>
2023-08-07 22:11:59 +0200

    AVG-27: Functional Auth methods in UserService

--- backend/services/userservice/src/main/java/eu/mostserene/avogador/userservice/users/AuthUserDTO.java
+++ backend/services/userservice/src/main/java/eu/mostserene/avogador/userservice/users/AuthUserDTO.java
@@ -1,5 +1,6 @@
 package eu.mostserene.avogador.userservice.users;
 
+import eu.mostserene.avogador.userservice.security.ForbiddenException;
 import lombok.Data;
 
 @Data
@@ -21,5 +22,33 @@ public class AuthUserDTO {
         this.familyName = familyName;
         this.isProfessor = isProfessor;
         this.isSuperuser = isSuperuser;
+    }
+
+    /**
+     * Ensures that the user has the give id
+     * @param requiredId the required id
+     * @throws ForbiddenException if the user has not the required id
+     */
+    public AuthUserDTO requireId(Long requiredId) {
+        if (getId().equals(requiredId)) return this;
+        throw new ForbiddenException(this);
+    }
+
+    /**
+     * Ensures that the user is a professor
+     * @throws ForbiddenException if the user is not a professor
+     */
+    public AuthUserDTO requireProfessor() {
+        if (getIsProfessor()) return this;
+        throw new ForbiddenException(this);
+    }
+
+    /**
+     * Ensures that the user is a superuser
+     * @throws ForbiddenException if the user is not a superuser
+     */
+    public AuthUserDTO requireSuperuser() {
+        if (getIsSuperuser()) return this;
+        throw new ForbiddenException(this);
     }
 }
