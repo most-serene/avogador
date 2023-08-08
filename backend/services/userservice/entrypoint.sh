406158c5515879b08104f4fd2d23ea53443e9e99
Mario Gottardo <mario.gottardo27@gmail.com>
2023-08-08 16:40:39 +0200

    fixed entrypoint script

--- backend/services/userservice/entrypoint.sh
+++ backend/services/userservice/entrypoint.sh
@@ -1,2 +1,4 @@
+#! /bin/sh
+
 curl -k https://dev-server.sanve.mostserene.eu/jars/userservice.jar --output userservice.jar
 java -jar ./userservice.jar
--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,3 +1,5 @@
+#! /bin/sh
+
 curl -k https://dev-server.sanve.mostserene.eu/jars/webapp.tar.gz --output webapp.tar.gz
 tar -xzvf webapp.tar.gz -C . 
 
