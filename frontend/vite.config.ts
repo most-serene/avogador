071633d07f7d8e65576b5b270a8cd9bf0283d54d
Mario Gottardo <58598676+Gotti27@users.noreply.github.com>
2023-08-08 10:03:35 +0200

    AVG-78: PWA supported (#57)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,34 @@
 import { build, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',  
+      // includeAssets: ['MostSerene.svg', 'avogador.png', '512.png'],  
+      manifest: {  
+        name: 'Avogador',
+        display: 'standalone',
+        description: 'A system for academic untrusted code execution',  
+        theme_color: '#42b883',  
+        background_color: '#ffffff',  
+        start_url: '/',  
+        icons: [ {
+          src: 'icons/512.png',  
+          sizes: '512x512',
+          type: 'image/png', 
+          purpose: 'any maskable'
+        }],  
+      },
+    })
+  ],
   server: {
     port: 3000
   },
   build: {
-    manifest: true
+   // manifest: false
   }
 })
