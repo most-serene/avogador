b340f08be064985e62fa2e1b13062aa0a386cd7e
Alessio-Campa <ale.campa00@gmail.com>
2023-08-10 16:54:34 +0200

    AVG.46 added UserController tests
    - added tests

--- backend/services/userservice/src/test/java/eu/mostserene/avogador/userservice/controllers/UserControllerTests.java
+++ backend/services/userservice/src/test/java/eu/mostserene/avogador/userservice/controllers/UserControllerTests.java
@@ -1,17 +1,22 @@
 package eu.mostserene.avogador.userservice.controllers;
 
+import eu.mostserene.avogador.userservice.apikey.ApiKey;
 import eu.mostserene.avogador.userservice.apikey.ApiKeyService;
 import eu.mostserene.avogador.userservice.mail.EmailService;
 import eu.mostserene.avogador.userservice.security.AuthServiceImpl;
 import eu.mostserene.avogador.userservice.users.*;
+import eu.mostserene.avogador.userservice.utils.NotFoundException;
+import io.jsonwebtoken.JwsHeader;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.*;
@@ -185,13 +190,190 @@ public class UserControllerTests {
 
     // skip google-auth
 
-
     @Test
     public void logoutUser() throws Exception {
         mvc.perform(get("/public/users/logout"))
                 .andDo(print())
-                .andExpect(cookie().exists("__Secure-jwt"));
+                .andExpect(cookie().exists("__Secure-jwt"))
+                .andExpect(cookie().maxAge("__Secure-jwt", 0));
     }
 
+    @Nested
+    class RevokeJWT {
+        @Test
+        public void fromStudent_get403() throws Exception {
+            when(authService.getRequestUser(any()))
+                    .thenReturn(student2Dto);
+
+            mvc.perform(patch("/public/users/1/revoke-jwt"))
+                    .andDo(print())
+                    .andExpect(status().isForbidden());
+        }
+
+        @Test
+        public void fromProfessor_get403() throws Exception {
+            when(authService.getRequestUser(any()))
+                    .thenReturn(professorDto);
+
+            mvc.perform(patch("/public/users/1/revoke-jwt"))
+                    .andDo(print())
+                    .andExpect(status().isForbidden());
+        }
+
+        @Test
+        public void fromSuperuser_get200() throws Exception {
+            when(authService.getRequestUser(any()))
+                    .thenReturn(superuserDto);
+
+            mvc.perform(patch("/public/users/1/revoke-jwt"))
+                    .andDo(print())
+                    .andExpect(status().isOk());
+        }
+
+        @Test
+        public void fromSelf_get200() throws Exception {
+            when(authService.getRequestUser(any()))
+                    .thenReturn(student1Dto);
+
+            mvc.perform(patch("/public/users/1/revoke-jwt"))
+                    .andDo(print())
+                    .andExpect(status().isOk());
+        }
+    }
+
+    @Nested
+    class GetApiKeys {
+        @Test
+        public void idMismatch_get403() throws Exception {
+            when(authService.getRequestUser(any()))
+                    .thenReturn(student2Dto);
+
+            mvc.perform(get("/public/users/1/api-key"))
+                    .andDo(print())
+                    .andExpect(status().isForbidden());
+        }
+
+        @Test
+        public void wrongId_get404() throws Exception {
+            when(authService.getRequestUser(any()))
+                    .thenReturn(student1Dto);
+            when(userService.getUserById(anyLong()))
+                    .thenReturn(Optional.empty());
+
+            mvc.perform(get("/public/users/1/api-key"))
+                    .andDo(print())
+                    .andExpect(status().isNotFound());
+        }
+
+        @Test
+        public void everythingRight_get200() throws Exception {
+            when(authService.getRequestUser(any()))
+                    .thenReturn(student1Dto);
+            when(userService.getUserById(anyLong()))
+                    .thenReturn(Optional.of(student1));
+            when(apiKeyService.getApiKeyByUser(any()))
+                    .thenReturn(List.of());
+
+            mvc.perform(get("/public/users/1/api-key"))
+                    .andDo(print())
+                    .andExpect(status().isOk());
+        }
+    }
+
+    @Nested
+    class GenerateApiKey {
+        @Test
+        public void mismatchId_get403() throws Exception {
+            when(authService.getRequestUser(any()))
+                    .thenReturn(student2Dto);
+
+            mvc.perform(post("/public/users/1/api-key")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("{\"name\": \"key\", \"expiration\": \"2016-02-16 11:00:02\"}"))
+                    .andDo(print())
+                    .andExpect(status().isForbidden());
+        }
+
+        @Test
+        public void wrongId_get404() throws Exception {
+            when(authService.getRequestUser(any()))
+                    .thenReturn(student1Dto);
+            when(userService.getUserById(anyLong()))
+                    .thenReturn(Optional.empty());
+
+            mvc.perform(post("/public/users/1/api-key")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("{\"name\": \"key\", \"expiration\": \"2016-02-16 11:00:02\"}"))
+                    .andDo(print())
+                    .andExpect(status().isNotFound());
+        }
+
+        @Test
+        public void everythingRight_get200() throws Exception {
+            when(authService.getRequestUser(any()))
+                    .thenReturn(student1Dto);
+            when(userService.getUserById(anyLong()))
+                    .thenReturn(Optional.of(student1));
+
+            mvc.perform(post("/public/users/1/api-key")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("{\"name\": \"key\", \"expiration\": \"2024-04-20T11:00:00Z\"}"))
+                    .andDo(print())
+                    .andExpect(status().isOk());
+        }
+    }
+
+    @Nested
+    class DeleteApiKey {
+        @Test
+        public void mismatchId_get403() throws Exception {
+            when(authService.getRequestUser(any()))
+                    .thenReturn(student2Dto);
+
+            mvc.perform(delete("/public/users/1/api-key/key"))
+                    .andDo(print())
+                    .andExpect(status().isForbidden());
+        }
+
+        @Test
+        public void wrongUserId_get404() throws Exception {
+            when(authService.getRequestUser(any()))
+                    .thenReturn(student1Dto);
+            when(userService.getUserById(anyLong()))
+                    .thenReturn(Optional.empty());
+
+            mvc.perform(delete("/public/users/1/api-key/key"))
+                    .andDo(print())
+                    .andExpect(status().isNotFound());
+        }
+
+        @Test
+        public void wrongKeyName_get404() throws Exception {
+            when(authService.getRequestUser(any()))
+                    .thenReturn(student1Dto);
+            when(userService.getUserById(anyLong()))
+                    .thenReturn(Optional.of(student1));
+            when(apiKeyService.getApiKeyByName(any(), anyString()))
+                    .thenReturn(Optional.empty());
+
+            mvc.perform(delete("/public/users/1/api-key/key"))
+                    .andDo(print())
+                    .andExpect(status().isNotFound());
+        }
+
+        @Test
+        public void everythingRight_get200() throws Exception {
+            when(authService.getRequestUser(any()))
+                    .thenReturn(student1Dto);
+            when(userService.getUserById(anyLong()))
+                    .thenReturn(Optional.of(student1));
+            when(apiKeyService.getApiKeyByName(any(), anyString()))
+                    .thenReturn(Optional.of(new ApiKey()));
+
+            mvc.perform(delete("/public/users/1/api-key/key"))
+                    .andDo(print())
+                    .andExpect(status().isOk());
+        }
+    }
 
 }
