240be5d160a1eb37a8d20e57543aef549f0401d5
Mario Gottardo <58598676+Gotti27@users.noreply.github.com>
2023-08-11 22:08:26 +0200

    AVG-60: Profile conf manager (#65)
    * AVG-60: Profile conf manager
    
    * AVG-60: minor fixes in manager
    
    * AVG-60: courseService profile manager
    
    * AVG-60: Course service profile manager
    
    * BadRequestException introduced in courseService

---
+++ backend/apigateway/src/main/java/eu/mostserene/avogador/apigateway/utils/AvogadorProfile.java
@@ -0,0 +1,14 @@
+package eu.mostserene.avogador.apigateway.utils;
+
+public enum AvogadorProfile {
+    DEVELOP("develop"),
+    TESTING("testing"),
+    STAGING("staging"),
+    PRODUCTION("production");
+
+    public final String profile;
+
+    AvogadorProfile(String profile) {
+        this.profile = profile;
+    }
+}
