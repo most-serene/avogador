3c76e271bb98f4a67534cb91f348d0c586e64777
Mario Gottardo <mario.gottardo27@gmail.com>
2023-08-15 22:57:58 +0200

    AVG-68: UserDTO parsing in header

---
+++ backend/services/courseservice/src/main/java/eu/mostserene/avogador/courseservice/security/converters/AuthUserDTOHeaderConverter.java
@@ -0,0 +1,23 @@
+package eu.mostserene.avogador.courseservice.security.converters;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import eu.mostserene.avogador.courseservice.users.UserDto;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+@Component
+public class AuthUserDTOHeaderConverter implements Converter<String, UserDto> {
+
+    @Override
+    public UserDto convert(@NonNull String source) {
+        try {
+            return new ObjectMapper().readValue(source, UserDto.class);
+        } catch (JsonProcessingException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error while parsing user header");
+        }
+    }
+}
