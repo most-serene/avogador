ab9ec13402f6b703d85910b9d0b86bd6893ea7f0
Mario Gottardo <mario.gottardo27@gmail.com>
2023-08-17 19:12:02 +0200

    AVG-66: dev utils improved

--- dev-utils.py
+++ dev-utils.py
@@ -12,11 +12,29 @@ class DevUtilsCmd(Cmd):
     def do_localDevelopMode(self, inp):
         branch = Repository('.').head.shorthand
         currDir = os.getcwd()
-        appscript.app('Terminal').do_script('cd ' + currDir + f'; BRANCH={branch} docker compose up --build')  # or any other command you choose
+        appscript.app('Terminal').do_script('cd ' + currDir + f'; BRANCH={branch} docker compose up --build')
         # os.system('docker compose up --build -d')
+
+    def do_stopLocalDevelopMode(self, inp):
+        os.system('BRANCH={branch} docker compose stop')
+    
+    def do_downLocalDevelopMode(self, inp):
+        os.system('BRANCH={branch} docker compose down')
+
+    def do_executeTests(self, inp):
+        os.system('cd backend/apigateway; gradle test')
+        os.system('cd backend/services/userservice; gradle test')
+        os.system('cd backend/services/courseservice; gradle test')
 
     def do_exit(self, inp):
         return True
+    
+    def do_start(self, inp: str):
+        branch = Repository('.').head.shorthand
+        if inp == '':
+            return
+        for s in inp.split(' '):
+            os.system(f'BRANCH={branch} docker compose start {s}')
     
     def do_restart(self, inp: str):
         branch = Repository('.').head.shorthand
@@ -24,6 +42,13 @@ class DevUtilsCmd(Cmd):
             return
         for s in inp.split(' '):
             os.system(f'BRANCH={branch} docker compose build {s} && BRANCH={branch} docker compose create {s} && BRANCH={branch} docker compose restart {s}')
+
+    def do_stop(self, inp: str):
+        branch = Repository('.').head.shorthand
+        if inp == '':
+            return
+        for s in inp.split(' '):
+            os.system(f'BRANCH={branch} docker compose stop {s}')
 
     def do_list_services(self, inp):
         branch = Repository('.').head.shorthand
@@ -42,6 +67,21 @@ class DevUtilsCmd(Cmd):
 
     def help_exit(self):
         print("exit the app")
+
+    def help_stopLocalDevelopMode(self):
+        print('stop the local backend cluster instance')
+
+    def help_downLocalDevelopMode(self):
+        print('remove the local backend cluster instance')
+
+    def help_executeTests(self):
+        print('execute all junit backend tests')
+
+    def help_start(self):
+        print('start the listed services')
+
+    def help_stop(self):
+        print('stop the listed services')
 
 
 p = DevUtilsCmd()
