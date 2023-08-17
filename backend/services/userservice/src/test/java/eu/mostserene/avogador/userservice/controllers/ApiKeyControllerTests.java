b128c78c917c5bdc84dd8658e7b5fc2dc50157c5
Alessio Campanelli <64778464+Alessio-Campa@users.noreply.github.com>
2023-08-17 12:04:08 +0200

    AVG-91 created ApiKeyController (#76)
    - moved api key endpoints to the new controller
    - moved tests

---
+++ backend/services/userservice/src/test/java/eu/mostserene/avogador/userservice/controllers/ApiKeyControllerTests.java
@@ -0,0 +1,168 @@
+package eu.mostserene.avogador.userservice.controllers;
+
+import eu.mostserene.avogador.userservice.apikey.ApiKey;
+import eu.mostserene.avogador.userservice.apikey.ApiKeyController;
+import eu.mostserene.avogador.userservice.apikey.ApiKeyService;
+import eu.mostserene.avogador.userservice.security.AuthService;
+import eu.mostserene.avogador.userservice.security.AuthServiceImpl;
+import eu.mostserene.avogador.userservice.users.*;
+import eu.mostserene.avogador.userservice.utils.ProfileManager;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(ApiKeyController.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class ApiKeyControllerTests {
+    private @Autowired MockMvc mvc;
+    private @MockBean AuthService authService;
+    private @MockBean UserService userService;
+    private @MockBean ApiKeyService apiKeyService;
+    private @MockBean ProfileManager profileManager;
+
+    private final User student1 = new User("student1@stud.unive.it", "Andy", "Bernard");
+    private final String student1Header = "{\"id\":1, \"email\":\"student1@stud.unive.it\", \"givenName\":\"Andy\", \"familyName\":\"Bernard\", \"isProfessor\":false, \"isSuperuser\":false}";
+
+    @Nested
+    class GetApiKeys {
+        @Test
+        public void idMismatch_get403() throws Exception {
+            mvc.perform(get("/public/users/2/api-key").header("User", student1Header))
+                    .andDo(print())
+                    .andExpect(status().isForbidden());
+        }
+
+        @Test
+        public void wrongId_get404() throws Exception {
+            when(userService.getUserById(anyLong()))
+                    .thenReturn(Optional.empty());
+
+            mvc.perform(get("/public/users/1/api-key").header("User", student1Header))
+                    .andDo(print())
+                    .andExpect(status().isNotFound());
+        }
+
+        @Test
+        public void everythingRight_get200() throws Exception {
+            when(userService.getUserById(anyLong()))
+                    .thenReturn(Optional.of(student1));
+            when(apiKeyService.getApiKeyByUser(any()))
+                    .thenReturn(List.of());
+
+            mvc.perform(get("/public/users/1/api-key").header("User", student1Header))
+                    .andDo(print())
+                    .andExpect(status().isOk());
+        }
+    }
+
+    @Nested
+    class GenerateApiKey {
+        @Test
+        public void mismatchId_get403() throws Exception {
+            mvc.perform(post("/public/users/2/api-key")
+                            .header("User", student1Header)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("{\"name\": \"key\", \"expiration\": \"2016-02-16 11:00:02\"}"))
+                    .andDo(print())
+                    .andExpect(status().isForbidden());
+        }
+
+        @Test
+        public void wrongId_get404() throws Exception {
+            when(userService.getUserById(anyLong()))
+                    .thenReturn(Optional.empty());
+
+            mvc.perform(post("/public/users/1/api-key")
+                            .header("User", student1Header)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("{\"name\": \"key\", \"expiration\": \"2016-02-16 11:00:02\"}"))
+                    .andDo(print())
+                    .andExpect(status().isNotFound());
+        }
+
+        @Test
+        public void nameWithSpaces_get400() throws Exception {
+            when(userService.getUserById(anyLong()))
+                    .thenReturn(Optional.empty());
+
+            mvc.perform(post("/public/users/1/api-key")
+                            .header("User", student1Header)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("{\"name\": \"key with spaces\", \"expiration\": \"2016-02-16 11:00:02\"}"))
+                    .andDo(print())
+                    .andExpect(status().isBadRequest());
+        }
+
+        @Test
+        public void everythingRight_get200() throws Exception {
+            when(userService.getUserById(anyLong()))
+                    .thenReturn(Optional.of(student1));
+
+            mvc.perform(post("/public/users/1/api-key")
+                            .header("User", student1Header)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("{\"name\": \"key\", \"expiration\": \"2024-04-20T11:00:00Z\"}"))
+                    .andDo(print())
+                    .andExpect(status().isOk());
+        }
+    }
+
+    @Nested
+    class DeleteApiKey {
+        @Test
+        public void mismatchId_get403() throws Exception {
+            mvc.perform(delete("/public/users/2/api-key/key").header("User", student1Header))
+                    .andDo(print())
+                    .andExpect(status().isForbidden());
+        }
+
+        @Test
+        public void wrongUserId_get404() throws Exception {
+            when(userService.getUserById(anyLong()))
+                    .thenReturn(Optional.empty());
+
+            mvc.perform(delete("/public/users/1/api-key/key").header("User", student1Header))
+                    .andDo(print())
+                    .andExpect(status().isNotFound());
+        }
+
+        @Test
+        public void wrongKeyName_get404() throws Exception {
+            when(userService.getUserById(anyLong()))
+                    .thenReturn(Optional.of(student1));
+            when(apiKeyService.getApiKeyByName(any(), anyString()))
+                    .thenReturn(Optional.empty());
+
+            mvc.perform(delete("/public/users/1/api-key/key").header("User", student1Header))
+                    .andDo(print())
+                    .andExpect(status().isNotFound());
+        }
+
+        @Test
+        public void everythingRight_get200() throws Exception {
+            when(userService.getUserById(anyLong()))
+                    .thenReturn(Optional.of(student1));
+            when(apiKeyService.getApiKeyByName(any(), anyString()))
+                    .thenReturn(Optional.of(new ApiKey()));
+
+            mvc.perform(delete("/public/users/1/api-key/key").header("User", student1Header))
+                    .andDo(print())
+                    .andExpect(status().isOk());
+        }
+    }
+
+}
--- backend/services/userservice/src/test/java/eu/mostserene/avogador/userservice/controllers/UserControllerTests.java
+++ backend/services/userservice/src/test/java/eu/mostserene/avogador/userservice/controllers/UserControllerTests.java
@@ -1,9 +1,7 @@
 package eu.mostserene.avogador.userservice.controllers;
 
-import eu.mostserene.avogador.userservice.apikey.ApiKey;
-import eu.mostserene.avogador.userservice.apikey.ApiKeyService;
 import eu.mostserene.avogador.userservice.mail.EmailService;
-import eu.mostserene.avogador.userservice.security.AuthServiceImpl;
+import eu.mostserene.avogador.userservice.security.AuthService;
 import eu.mostserene.avogador.userservice.users.*;
 import eu.mostserene.avogador.userservice.utils.ProfileManager;
 import org.junit.jupiter.api.Nested;
@@ -12,14 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.Duration;
-import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
 import static org.mockito.ArgumentMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -32,10 +27,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class UserControllerTests {
     private @Autowired MockMvc mvc;
     private @MockBean UserRepository repository;
-    private @MockBean AuthServiceImpl authService;
+    private @MockBean AuthService authService;
     private @MockBean EmailService emailService;
     private @MockBean UserService userService;
-    private @MockBean ApiKeyService apiKeyService;
     private @MockBean ProfileManager profileManager;
 
 
@@ -222,133 +216,4 @@ public class UserControllerTests {
                     .andExpect(status().isOk());
         }
     }
-
-    @Nested
-    class GetApiKeys {
-        @Test
-        public void idMismatch_get403() throws Exception {
-            mvc.perform(get("/public/users/2/api-key").header("User", student1Header))
-                    .andDo(print())
-                    .andExpect(status().isForbidden());
-        }
-
-        @Test
-        public void wrongId_get404() throws Exception {
-            when(userService.getUserById(anyLong()))
-                    .thenReturn(Optional.empty());
-
-            mvc.perform(get("/public/users/1/api-key").header("User", student1Header))
-                    .andDo(print())
-                    .andExpect(status().isNotFound());
-        }
-
-        @Test
-        public void everythingRight_get200() throws Exception {
-            when(userService.getUserById(anyLong()))
-                    .thenReturn(Optional.of(student1));
-            when(apiKeyService.getApiKeyByUser(any()))
-                    .thenReturn(List.of());
-
-            mvc.perform(get("/public/users/1/api-key").header("User", student1Header))
-                    .andDo(print())
-                    .andExpect(status().isOk());
-        }
-    }
-
-    @Nested
-    class GenerateApiKey {
-        @Test
-        public void mismatchId_get403() throws Exception {
-            mvc.perform(post("/public/users/2/api-key")
-                            .header("User", student1Header)
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content("{\"name\": \"key\", \"expiration\": \"2016-02-16 11:00:02\"}"))
-                    .andDo(print())
-                    .andExpect(status().isForbidden());
-        }
-
-        @Test
-        public void wrongId_get404() throws Exception {
-            when(userService.getUserById(anyLong()))
-                    .thenReturn(Optional.empty());
-
-            mvc.perform(post("/public/users/1/api-key")
-                            .header("User", student1Header)
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content("{\"name\": \"key\", \"expiration\": \"2016-02-16 11:00:02\"}"))
-                    .andDo(print())
-                    .andExpect(status().isNotFound());
-        }
-
-        @Test
-        public void nameWithSpaces_get400() throws Exception {
-            when(userService.getUserById(anyLong()))
-                    .thenReturn(Optional.empty());
-
-            mvc.perform(post("/public/users/1/api-key")
-                            .header("User", student1Header)
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content("{\"name\": \"key with spaces\", \"expiration\": \"2016-02-16 11:00:02\"}"))
-                    .andDo(print())
-                    .andExpect(status().isBadRequest());
-        }
-
-        @Test
-        public void everythingRight_get200() throws Exception {
-            when(userService.getUserById(anyLong()))
-                    .thenReturn(Optional.of(student1));
-
-            mvc.perform(post("/public/users/1/api-key")
-                            .header("User", student1Header)
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content("{\"name\": \"key\", \"expiration\": \"2024-04-20T11:00:00Z\"}"))
-                    .andDo(print())
-                    .andExpect(status().isOk());
-        }
-    }
-
-    @Nested
-    class DeleteApiKey {
-        @Test
-        public void mismatchId_get403() throws Exception {
-            mvc.perform(delete("/public/users/2/api-key/key").header("User", student1Header))
-                    .andDo(print())
-                    .andExpect(status().isForbidden());
-        }
-
-        @Test
-        public void wrongUserId_get404() throws Exception {
-            when(userService.getUserById(anyLong()))
-                    .thenReturn(Optional.empty());
-
-            mvc.perform(delete("/public/users/1/api-key/key").header("User", student1Header))
-                    .andDo(print())
-                    .andExpect(status().isNotFound());
-        }
-
-        @Test
-        public void wrongKeyName_get404() throws Exception {
-            when(userService.getUserById(anyLong()))
-                    .thenReturn(Optional.of(student1));
-            when(apiKeyService.getApiKeyByName(any(), anyString()))
-                    .thenReturn(Optional.empty());
-
-            mvc.perform(delete("/public/users/1/api-key/key").header("User", student1Header))
-                    .andDo(print())
-                    .andExpect(status().isNotFound());
-        }
-
-        @Test
-        public void everythingRight_get200() throws Exception {
-            when(userService.getUserById(anyLong()))
-                    .thenReturn(Optional.of(student1));
-            when(apiKeyService.getApiKeyByName(any(), anyString()))
-                    .thenReturn(Optional.of(new ApiKey()));
-
-            mvc.perform(delete("/public/users/1/api-key/key").header("User", student1Header))
-                    .andDo(print())
-                    .andExpect(status().isOk());
-        }
-    }
-
 }
