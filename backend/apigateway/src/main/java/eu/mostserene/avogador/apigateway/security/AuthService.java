0330c29a97b230311c89a11be1ac90ee826218fa
Mario Gottardo <58598676+Gotti27@users.noreply.github.com>
2023-08-19 00:12:45 +0200

    AVG-111: RestTemplate in gateway calls (#87)

--- backend/apigateway/src/main/java/eu/mostserene/avogador/apigateway/security/AuthService.java
+++ backend/apigateway/src/main/java/eu/mostserene/avogador/apigateway/security/AuthService.java
@@ -1,15 +1,8 @@
 package eu.mostserene.avogador.apigateway.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.Optional;
 
 @Component
@@ -17,46 +10,18 @@ import java.util.Optional;
 public class AuthService {
 
     public Optional<AuthUserDTO> decodeAndValidateJWT(String jwtToken) {
-        var a = buildAuthRequest("validate-jwt", jwtToken);
-        if (a.isPresent()) {
-            return sendAuthRequest(a.get());
-        }
-        return Optional.empty();
+        AuthUserDTO user = new RestTemplateBuilder()
+                .build()
+                .postForObject("http://users/security/validate-jwt", jwtToken, AuthUserDTO.class);
+
+        return user != null ? Optional.of(user) : Optional.empty();
     }
 
     public Optional<AuthUserDTO> decodeAndValidateApiKey(String apiKey) {
-        var a = buildAuthRequest("validate-key", apiKey);
-        if (a.isPresent()) {
-            return sendAuthRequest(a.get());
-        }
-        return Optional.empty();
-    }
+        AuthUserDTO user = new RestTemplateBuilder()
+                .build()
+                .postForObject("http://users/security/validate-key", apiKey, AuthUserDTO.class);
 
-    private Optional<HttpRequest> buildAuthRequest(String authEndpoint, String claim) {
-        try {
-            log.info(new URI("http://users/security/" + authEndpoint).toString());
-            return Optional.of(HttpRequest
-                    .newBuilder()
-                    .uri(new URI("http://users/security/" + authEndpoint))
-                    .header("Content-Type", "text/plain")
-                    .POST(HttpRequest.BodyPublishers.ofString(claim))
-                    .build());
-        } catch (URISyntaxException e) {
-            log.error(e.getMessage());
-            return Optional.empty();
-        }
-    }
-
-    private Optional<AuthUserDTO> sendAuthRequest(HttpRequest httpRequest) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            return Optional.of(objectMapper.readValue(
-                    HttpClient.newHttpClient()
-                            .send(httpRequest, HttpResponse.BodyHandlers.ofString())
-                            .body(), AuthUserDTO.class));
-        } catch (IOException | InterruptedException e) {
-            log.error(e.getMessage());
-            return Optional.empty();
-        }
+        return user != null ? Optional.of(user) : Optional.empty();
     }
 }
