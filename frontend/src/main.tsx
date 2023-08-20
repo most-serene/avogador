12f6bf14a375b467733fcbf707951db3d36cf82c
Mario Gottardo <mario.gottardo27@gmail.com>
2023-08-20 19:40:05 +0200

    DatePicker and date-fns LocalizationProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,8 @@ import App from "./App";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { lightTheme } from "./themes.ts";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -15,14 +17,16 @@ const root = document.getElementById("root");
 if (root) {
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
-      <ThemeProvider theme={lightTheme}>
-        <CssBaseline />
-        <GoogleOAuthProvider
-          clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID as string}
-        >
-          <App />
-        </GoogleOAuthProvider>
-      </ThemeProvider>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <ThemeProvider theme={lightTheme}>
+          <CssBaseline />
+          <GoogleOAuthProvider
+            clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID as string}
+          >
+            <App />
+          </GoogleOAuthProvider>
+        </ThemeProvider>
+      </LocalizationProvider>
     </React.StrictMode>,
   );
 }
