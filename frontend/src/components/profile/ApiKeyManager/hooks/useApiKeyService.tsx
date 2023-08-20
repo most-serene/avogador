4006895078b1247c9cda6e9d60336b8158bb134b
Mario Gottardo <mario.gottardo27@gmail.com>
2023-08-20 19:41:11 +0200

    AVG-84: fix apikeyservice hook re-render

--- frontend/src/components/profile/ApiKeyManager/hooks/useApiKeyService.tsx
+++ frontend/src/components/profile/ApiKeyManager/hooks/useApiKeyService.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { useAvogadorApi } from "../../../../hooks/useAvogadorApi";
 import { User } from "../../../authentication/types";
 import { ApiKey } from "../types";
@@ -5,37 +6,40 @@ import { ApiKey } from "../types";
 const useApiKeyService = () => {
   const avogadorApi = useAvogadorApi();
 
-  const getUserApiKeys: (user: User) => Promise<ApiKey[]> = async (
-    user: User,
-  ) => {
-    const { data }: { data: ApiKey[] } = await avogadorApi.get(
-      `/public/users/${user.id}/api-key`,
-    );
-    return data;
-  };
+  const getUserApiKeys: (user: User) => Promise<ApiKey[]> = useCallback(
+    async (user: User) => {
+      const { data }: { data: ApiKey[] } = await avogadorApi.get(
+        `/users/${user.id}/api-key`,
+      );
+      return data;
+    },
+    [avogadorApi],
+  );
 
   const createUserApiKey: (
     user: User,
     keyName: string,
     expiration: Date,
-  ) => Promise<string> = async (
-    user: User,
-    keyName: string,
-    expiration: Date,
-  ) => {
-    const { data: key }: { data: string } = await avogadorApi.post(
-      `/public/users/${user.id}/api-key`,
-      {
-        name: keyName,
-        expiration: expiration,
-      },
-    );
-    return key;
-  };
+  ) => Promise<string> = useCallback(
+    async (user: User, keyName: string, expiration: Date) => {
+      const { data: key }: { data: string } = await avogadorApi.post(
+        `/users/${user.id}/api-key`,
+        {
+          name: keyName,
+          expiration: expiration,
+        },
+      );
+      return key;
+    },
+    [avogadorApi],
+  );
 
-  const deleteUserApiKey = (user: User, keyName: string) => {
-    return avogadorApi.delete(`/public/users/${user.id}/api-key/${keyName}`);
-  };
+  const deleteUserApiKey = useCallback(
+    (user: User, keyName: string) => {
+      return avogadorApi.delete(`/users/${user.id}/api-key/${keyName}`);
+    },
+    [avogadorApi],
+  );
 
   return {
     getUserApiKeys,
