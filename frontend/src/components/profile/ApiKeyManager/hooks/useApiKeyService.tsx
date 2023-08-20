885d23b9fc3673e2a6d912a4057b03c92725d249
Mario Gottardo <mario.gottardo27@gmail.com>
2023-08-20 11:14:34 +0200

    AVG-84: API key service hook

---
+++ frontend/src/components/profile/ApiKeyManager/hooks/useApiKeyService.tsx
@@ -0,0 +1,52 @@
+import { useAvogadorApi } from "../../../../hooks/useAvogadorApi";
+import { User } from "../../../authentication/types";
+import { ApiKey } from "../types";
+
+const useApiKeyService = () => {
+  const avogadorApi = useAvogadorApi();
+
+  const getUserApiKeys: (user: User) => Promise<ApiKey[]> = async (
+    user: User,
+  ) => {
+    const { data }: { data: ApiKey[] } = await avogadorApi.get(
+      `/public/users/${user.id}/api-key`,
+    );
+    return data;
+  };
+
+  const createUserApiKey: (
+    user: User,
+    keyName: string,
+    expiration: Date,
+  ) => Promise<string> = async (
+    user: User,
+    keyName: string,
+    expiration: Date,
+  ) => {
+    const { data: key }: { data: string } = await avogadorApi.post(
+      `/public/users/${user.id}/api-key`,
+      {
+        name: keyName,
+        expiration: expiration,
+      },
+    );
+    return key;
+  };
+
+  const deleteUserApiKey = (
+    user: User,
+    keyName: string
+  ) => {
+    return avogadorApi.delete(
+      `/public/users/${user.id}/api-key/${keyName}`
+    );
+  };
+
+  return {
+    getUserApiKeys,
+    createUserApiKey,
+    deleteUserApiKey,
+  };
+};
+
+export default useApiKeyService;
