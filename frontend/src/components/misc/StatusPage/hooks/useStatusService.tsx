eb97a503d4253ef6f49f4d9a94af2901c3c14a67
Mario Gottardo <58598676+Gotti27@users.noreply.github.com>
2023-08-21 19:40:40 +0200

    AVG-121: status page refactor (#89)
    * AVG-123: status responses unification
    
    * AVG-122: frontend status page
    
    * AVG-122: added gateway version number

---
+++ frontend/src/components/misc/StatusPage/hooks/useStatusService.tsx
@@ -0,0 +1,22 @@
+import { useCallback } from "react";
+import { useAvogadorApi } from "../../../../hooks/useAvogadorApi";
+import { MicroService } from "../types";
+
+export const useStatusService = () => {
+  const avogadorApi = useAvogadorApi();
+
+  const getMicroservicesStatus = useCallback(async () => {
+    const { data }: { data: MicroService[] } = await avogadorApi.get("/status");
+    return data;
+  }, [avogadorApi]);
+
+  const getBackendVersion = useCallback(async () => {
+    const { data: version }: { data: string } = await avogadorApi.get("/");
+    return version;
+  }, [avogadorApi]);
+
+  return {
+    getMicroservicesStatus,
+    getBackendVersion,
+  };
+};
