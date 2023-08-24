6c104638adcef8b57d59e026f329eda59601b96e
Mario Gottardo <mario.gottardo27@gmail.com>
2023-08-24 23:11:19 +0200

    AVG-114: configure log only on deploy

--- frontend/src/conf/Glitchtip.ts
+++ frontend/src/conf/Glitchtip.ts
@@ -1,6 +1,8 @@
 import * as Sentry from "@sentry/browser";
 
 export const connectToGlitchTip = () => {
+  if (!import.meta.env.PROD) return;
+
   Sentry.init({
     dsn: import.meta.env.VITE_GLITCHTIP_DSN as string,
     release: import.meta.env.APP_VERSION as string,
