1f189a1bb710abfb8d192a4b526b6ab3a6f7cba7
Mario Gottardo <mario.gottardo27@gmail.com>
2023-08-24 23:01:53 +0200

    AVG-114: Glitchtip linked to React

---
+++ frontend/src/conf/Glitchtip.ts
@@ -0,0 +1,10 @@
+import * as Sentry from "@sentry/browser";
+
+export const connectToGlitchTip = () => {
+  Sentry.init({
+    dsn: import.meta.env.VITE_GLITCHTIP_DSN as string,
+    release: import.meta.env.APP_VERSION as string,
+    environment: import.meta.env.MODE,
+    autoSessionTracking: false,
+  });
+};
