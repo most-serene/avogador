27f8bd1344e563a7dc492ddc1e52421746c116e6
Mario Gottardo <mario.gottardo27@gmail.com>
2023-08-25 11:16:22 +0200

    AVG-115: API Gateway linked to GlitchTip

--- backend/apigateway/src/main/java/eu/mostserene/avogador/apigateway/ApiGatewayApplication.java
+++ backend/apigateway/src/main/java/eu/mostserene/avogador/apigateway/ApiGatewayApplication.java
@@ -2,8 +2,14 @@ package eu.mostserene.avogador.apigateway;
 
 import eu.mostserene.avogador.apigateway.status.MicroServiceStatus;
 import eu.mostserene.avogador.apigateway.status.StatusService;
+import eu.mostserene.avogador.apigateway.utils.LoggerColors;
+import eu.mostserene.avogador.apigateway.utils.ProfileManager;
+import io.sentry.Sentry;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.info.BuildProperties;
@@ -20,11 +26,17 @@ import java.util.List;
 @Slf4j
 public class ApiGatewayApplication {
 
+    @Value("${sentry.dsn}")
+    private String sentryDSN;
+
     @Autowired
     private BuildProperties buildProperties;
 
     @Autowired
     private StatusService statusService;
+
+    @Autowired
+    private ProfileManager profileManager;
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
@@ -52,6 +64,29 @@ public class ApiGatewayApplication {
     @GetMapping("/status")
     List<MicroServiceStatus> getStatus() {
         return statusService.getMicroservicesStatus();
+    }
+
+    @PostConstruct
+    private void postConstruct() {
+        String profile = profileManager.getActiveProfiles();
+
+        if ("staging".equals(profile) || "production".equals(profile)) {
+            Sentry.init(options -> {
+                options.setDsn(sentryDSN);
+                options.setServerName(buildProperties.getName());
+                options.setRelease(buildProperties.getVersion());
+                options.setAttachStacktrace(true);
+                options.setEnvironment(profileManager.getActiveProfiles());
+            });
+        } else {
+            log.info(LoggerColors.warn("Remote Logger not active on develop - testing modes"));
+        }
+    }
+
+    @PreDestroy
+    private void closeSentry() {
+        Sentry.close();
+        log.info(LoggerColors.cyan("Sentry closed"));
     }
 
     public static void main(String[] args) {
