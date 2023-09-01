50a2498258cf23234924daddf5edc43ba7e5e445
Mario Gottardo <mario.gottardo27@gmail.com>
2023-09-01 10:27:50 +0200

    AVG-143: CSRF protection reactivation

--- frontend/src/hooks/useAvogadorApi.tsx
+++ frontend/src/hooks/useAvogadorApi.tsx
@@ -2,10 +2,17 @@ import axios from "axios";
 import { atom, useAtom } from "jotai";
 
 const avogadorApiClientAtom = atom(() => {
-  return axios.create({
+  const client = axios.create({
     baseURL: `${import.meta.env.VITE_AVOGADOR_BACKEND_API_ADDRESS}`,
     withCredentials: true,
   });
+
+  const storedCSRF = localStorage.getItem("Jwt-CSRF-Hash");
+  if (storedCSRF !== null) {
+    client.defaults.headers.common["Jwt-CSRF-Hash"] = storedCSRF;
+  }
+
+  return client;
 });
 
 export const useAvogadorApi = () => {
