89f43c07605c109fcd42082c180dff393b1503fe
Mario Gottardo <mario.gottardo27@gmail.com>
2023-09-07 15:44:52 +0200

    AVG-136: course folder creation handler

---
+++ backend/services/filesystemservice/src/main/java/eu/mostserene/avogador/filesystemservice/utils/ServerInitError.java
@@ -0,0 +1,11 @@
+package eu.mostserene.avogador.filesystemservice.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ServerInitError extends Error {
+    public ServerInitError(String message) {
+        super(message);
+        log.error(LoggerColors.error(message));
+    }
+}
