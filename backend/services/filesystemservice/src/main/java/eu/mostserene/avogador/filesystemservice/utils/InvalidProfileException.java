46efb644b6b3eb4e0301cfb7841e575f9b77cd7f
Mario Gottardo <58598676+Gotti27@users.noreply.github.com>
2023-09-10 17:55:13 +0200

    AVG-161: fs-service sentry and security config (#132)

---
+++ backend/services/filesystemservice/src/main/java/eu/mostserene/avogador/filesystemservice/utils/InvalidProfileException.java
@@ -0,0 +1,14 @@
+package eu.mostserene.avogador.filesystemservice.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Thrown if the provided Profile is invalid
+ */
+@Slf4j
+public class InvalidProfileException extends Exception {
+    public InvalidProfileException() {
+        super("No profile has been provided");
+        log.error("----- NO PROFILE HAS BEEN PROVIDED -----");
+    }
+}
