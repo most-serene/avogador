677d26e63cf80e1e660474eeb9a745429ce826b9
Mario Gottardo <mario.gottardo27@gmail.com>
2023-09-11 11:05:00 +0200

    AVG-175: exercise service created

---
+++ backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/utils/NotFoundException.java
@@ -0,0 +1,15 @@
+package eu.mostserene.avogador.exerciseservice.utils;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String resource) {
+        super(resource + " not found");
+    }
+    public NotFoundException() {
+        super("Resource not found");
+    }
+
+}
