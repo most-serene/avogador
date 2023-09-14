9b30a4d1aedd756c84ab2de144d938fd80b6b233
Alessio Campanelli <64778464+Alessio-Campa@users.noreply.github.com>
2023-09-14 16:08:14 +0200

    AVG-88 UserServiceImpl cleanup (#145)

--- backend/services/courseservice/src/main/java/eu/mostserene/avogador/courseservice/users/UserService.java
+++ backend/services/courseservice/src/main/java/eu/mostserene/avogador/courseservice/users/UserService.java
@@ -9,8 +9,8 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface UserService {
-    UserDto getUser(UUID id) throws NotFoundException;
-    UserDto getUser(String email);
+    // UserDto getUser(UUID id) throws NotFoundException;
+    // UserDto getUser(String email);
     String getRequestID(HttpServletRequest request);
     List<UserDto> getUsersFromIdList(List<UUID> ids);
     List<UserDto> getUsersFromIdList(List<UUID> ids, Optional<Integer> limit, Optional<Integer> offset, Optional<String> orderBy, Optional<String> direction);
