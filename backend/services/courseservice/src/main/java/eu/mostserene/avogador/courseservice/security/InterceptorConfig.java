cc99a2d6133d43e28baeebddbbb7170edc3e4249
Mario Gottardo <mario.gottardo27@gmail.com>
2023-09-14 15:27:22 +0200

    AVG-105: public rest api annotation in courses

--- backend/services/courseservice/src/main/java/eu/mostserene/avogador/courseservice/security/InterceptorConfig.java
+++ backend/services/courseservice/src/main/java/eu/mostserene/avogador/courseservice/security/InterceptorConfig.java
@@ -1,5 +1,6 @@
 package eu.mostserene.avogador.courseservice.security;
 
+import eu.mostserene.avogador.courseservice.security.restapicontrol.RequestSourceInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -8,10 +9,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Component
 public class InterceptorConfig implements WebMvcConfigurer {
     @Autowired
-    LoggerInterceptor loggerInterceptor;
+    private LoggerInterceptor loggerInterceptor;
+
+    @Autowired
+    private RequestSourceInterceptor requestSourceInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loggerInterceptor);
+        registry.addInterceptor(requestSourceInterceptor);
     }
 }
