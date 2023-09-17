e04597647158f44ad89521281b70161e1f15e706
Mario Gottardo <mario.gottardo27@gmail.com>
2023-09-17 18:40:13 +0200

    JoinCourse code cleanup

--- frontend/src/components/courses/JoinCourse/JoinCourse.tsx
+++ frontend/src/components/courses/JoinCourse/JoinCourse.tsx
@@ -46,12 +46,12 @@ const JoinCourse = () => {
         }
       })
       .catch((e) => {
-        console.log(e);
+        console.error(e);
+        enqueueSnackbar("An error has occurred", { variant: "error" });
       });
   };
 
   useEffect(() => {
-    console.log(courseId);
     if (courseId === undefined) return;
 
     getCourseById(courseId)
