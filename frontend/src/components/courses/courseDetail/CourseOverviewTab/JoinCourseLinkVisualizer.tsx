0913b80fed1f4720ba6c2d1c83f47652440b4a63
Mario Gottardo <mario.gottardo27@gmail.com>
2023-09-17 18:45:36 +0200

    AVG-185: Show Join Link restyle and QR code

--- frontend/src/components/courses/courseDetail/CourseOverviewTab/JoinCourseLinkVisualizer.tsx
+++ frontend/src/components/courses/courseDetail/CourseOverviewTab/JoinCourseLinkVisualizer.tsx
@@ -1,106 +1,88 @@
 import { GetCoursesDetailResponse } from "@courses/types";
 import Box from "@mui/material/Box";
-import {
-  Button,
-  Card,
-  CardContent,
-  Modal,
-  Paper,
-  Typography,
-} from "@mui/material";
+import { Button, Card, CardContent, Typography } from "@mui/material";
 import { enqueueSnackbar } from "notistack";
 import { useState } from "react";
+import { QrCode, Share } from "@mui/icons-material";
+import { getJoinLink } from "@courses/courseDetail/CourseOverviewTab/utils";
+import JoinCourseQRCodeModal from "@courses/courseDetail/CourseOverviewTab/JoinCourseQRCodeModal";
+import InfoPoint from "@structure/InfoPoint/InfoPoint";
 
 interface JoinCourseLinkVisualizerProps {
   course: GetCoursesDetailResponse | undefined;
 }
+const copyJoinLink = (course: GetCoursesDetailResponse) => {
+  navigator.clipboard
+    .writeText(getJoinLink(course))
+    .then(() =>
+      enqueueSnackbar({
+        message: "Join code copied to the clipboard",
+        variant: "success",
+      }),
+    )
+    .catch(() => {
+      enqueueSnackbar({
+        message: "Failed to copy join link",
+        variant: "error",
+      });
+    });
+};
 
 const JoinCourseLinkVisualizer = ({
   course,
 }: JoinCourseLinkVisualizerProps) => {
-  const [showJoinCode, setShowJoinCode] = useState(false);
-
-  const modalStyle = {
-    position: "absolute" as const,
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    bgcolor: "background.paper",
-    borderRadius: 1,
-    boxShadow: 24,
-    p: 4,
-  };
+  const [showQrJoinCode, setShowQrJoinCode] = useState(false);
 
   return (
     <Card sx={{ width: "32rem" }} raised>
       <CardContent>
+        <Box display={"flex"} justifyContent={"space-between"}>
+          <Typography
+            variant="h5"
+            color="text.secondary"
+            display={"inline"}
+            gutterBottom
+          >
+            Join course link
+          </Typography>
+          <InfoPoint
+            message={
+              "Share this join code with your students to let them join this course."
+            }
+          />
+        </Box>
         <Box display="flex" justifyContent="center" marginTop=".5rem">
           <Button
-            size={"large"}
             variant={"outlined"}
+            sx={{ mr: ".5rem" }}
+            disabled={course === undefined}
             onClick={() => {
-              setShowJoinCode(!showJoinCode);
+              if (course) copyJoinLink(course);
             }}
-            sx={{ ml: ".5rem" }}
           >
-            show join code
+            <Share /> Copy join link
+          </Button>
+
+          <Button
+            variant={"outlined"}
+            sx={{ ml: ".5rem" }}
+            disabled={course === undefined}
+            onClick={() => {
+              setShowQrJoinCode(true);
+            }}
+          >
+            <QrCode /> Generate QR code
           </Button>
         </Box>
       </CardContent>
 
-      <Modal
-        open={showJoinCode}
-        onClose={() => {
-          setShowJoinCode(false);
-        }}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={modalStyle}>
-          <Typography variant="body1" gutterBottom>
-            Share this join code with your students to let them join this
-            course.
-          </Typography>
-          <Paper variant={"outlined"}>
-            <Typography
-              display={"flex"}
-              justifyContent={"center"}
-              variant="caption"
-            >
-              {course?.joinCode}
-            </Typography>
-          </Paper>
-          <Box display="flex" justifyContent="center" marginTop=".5rem">
-            <Button
-              variant={"outlined"}
-              sx={{ mr: ".5rem" }}
-              onClick={() => {
-                navigator.clipboard
-                  .writeText(
-                    `https://${window.location.hostname}/courses/${course?.id}/join?code=${course?.joinCode}`,
-                  )
-                  .then(() =>
-                    enqueueSnackbar({
-                      message: "Join code copied to the clipboard",
-                      variant: "success",
-                    }),
-                  )
-                  .catch(() => {
-                    enqueueSnackbar({
-                      message: "Failed to copy join link",
-                      variant: "error",
-                    });
-                  });
-              }}
-            >
-              Copy join link
-            </Button>
-            <Button variant={"outlined"} sx={{ ml: ".5rem" }}>
-              Generate QR code
-            </Button>
-          </Box>
-        </Box>
-      </Modal>
+      {course && (
+        <JoinCourseQRCodeModal
+          course={course}
+          showQrJoinCode={showQrJoinCode}
+          setShowQrJoinCode={setShowQrJoinCode}
+        />
+      )}
     </Card>
   );
 };
