7cdf12809a6cf5e7917271c646e9b54287b87e90
Mario Gottardo <mario.gottardo27@gmail.com>
2023-09-18 09:26:16 +0200

    AVG-211: CourseTrialsTab creation

---
+++ frontend/src/components/courses/courseDetail/CourseTrialsTab/CourseTrialsTab.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const CourseTrialsTab = () => {
+  return (
+    <Typography variant={"h4"} color={"secondary"} align={"center"}>
+      Tests Tab Coming Soon
+    </Typography>
+  );
+};
+
+export default CourseTrialsTab;
