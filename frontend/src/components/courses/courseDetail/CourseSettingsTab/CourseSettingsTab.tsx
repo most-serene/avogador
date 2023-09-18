484573d79f1139ab8632e3172ccfaa62c21ed037
Mario Gottardo <58598676+Gotti27@users.noreply.github.com>
2023-09-18 13:00:56 +0200

    AVG-211: CourseDetailScreen tab param (#156)

---
+++ frontend/src/components/courses/courseDetail/CourseSettingsTab/CourseSettingsTab.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const CourseSettingsTab = () => {
+  return (
+    <Typography variant={"h4"} color={"secondary"} align={"center"}>
+      Settings Tab Coming Soon
+    </Typography>
+  );
+};
+
+export default CourseSettingsTab;
