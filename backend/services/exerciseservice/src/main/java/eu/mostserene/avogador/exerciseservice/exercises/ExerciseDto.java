9558e0ef1afcef659cd019e268688a2da33f0ad7
Mario Gottardo <mario.gottardo27@gmail.com>
2023-09-19 09:21:10 +0200

    AVG-203: ExerciseDto class

---
+++ backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/exercises/ExerciseDto.java
@@ -0,0 +1,29 @@
+package eu.mostserene.avogador.exerciseservice.exercises;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class ExerciseDto {
+    private UUID id;
+
+    private UUID trialId;
+
+    private String name;
+
+    private String statement;
+
+    private Integer timeLimit;
+
+    private Boolean isVisible;
+
+    public ExerciseDto(UUID id, UUID trialId, String name, String statement, Integer timeLimit, Boolean isVisible) {
+        this.id = id;
+        this.trialId = trialId;
+        this.name = name;
+        this.statement = statement;
+        this.timeLimit = timeLimit;
+        this.isVisible = isVisible;
+    }
+}
