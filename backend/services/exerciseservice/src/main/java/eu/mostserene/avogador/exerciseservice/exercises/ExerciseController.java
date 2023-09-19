44f1ac99542b5b18b1a314b268462bd28fc6e96b
Mario Gottardo <mario.gottardo27@gmail.com>
2023-09-19 09:21:47 +0200

    AVG-203: ExerciseController skeleton

---
+++ backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/exercises/ExerciseController.java
@@ -0,0 +1,58 @@
+package eu.mostserene.avogador.exerciseservice.exercises;
+
+import eu.mostserene.avogador.exerciseservice.users.UserDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/public/exercises")
+@Slf4j
+public class ExerciseController {
+
+    /**
+     * Returns the exercise given the exercise ID
+     * @param user the requesting user
+     * @param exerciseId the id of the exercise
+     * @return the exercise
+     */
+    @GetMapping("/{exerciseId}")
+    private Exercise getExerciseById(@RequestHeader(name = "User") UserDto user, @PathVariable UUID exerciseId) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Creates an exercise in a given trial
+     * @param user the requesting user
+     * @param exercise the DTO of the exercise to create
+     * @return the created exercise
+     */
+    @PostMapping("")
+    private Exercise createExercise(@RequestHeader(name = "User") UserDto user, @RequestBody ExerciseDto exercise) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Updates an existing exercise
+     * @param user the requesting user
+     * @param exerciseId the id of the exercise to update
+     * @param exercise the updated exercise
+     * @return the saved updated exercise
+     */
+    @PutMapping("/{exerciseId}")
+    private Exercise updateExercise(@RequestHeader(name = "User") UserDto user, @PathVariable UUID exerciseId, @RequestBody ExerciseDto exercise) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Deletes an exercise
+     * @param user the requesting exercise
+     * @param exerciseId the id of the exercise
+     * @return the deleted exercise
+     */
+    @DeleteMapping("/{exerciseId}")
+    private Exercise deleteExercise(@RequestHeader(name = "User") UserDto user, @PathVariable UUID exerciseId) {
+        throw new UnsupportedOperationException();
+    }
+}
