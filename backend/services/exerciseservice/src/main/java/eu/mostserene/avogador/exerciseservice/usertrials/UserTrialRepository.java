ba512a97cdc78b6c9a48eef990939ff322d310d1
Alessio-Campa <ale.campa00@gmail.com>
2023-09-20 13:02:03 +0200

    AVG-200 Added UserTrialServiceImpl

--- backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/usertrials/UserTrialRepository.java
+++ backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/usertrials/UserTrialRepository.java
@@ -2,7 +2,10 @@ package eu.mostserene.avogador.exerciseservice.usertrials;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UserTrialRepository extends JpaRepository<UserTrial, UUID> {
+    List<UserTrial> findByUserId(UUID userId);
+    List<UserTrial> findByTrial_Id(UUID id);
 }
