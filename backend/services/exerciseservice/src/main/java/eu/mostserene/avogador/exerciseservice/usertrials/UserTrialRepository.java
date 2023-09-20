f600f25dee07cf73bc9e06a7192a1e2cdf907088
Mario Gottardo <mario.gottardo27@gmail.com>
2023-09-20 19:14:06 +0200

    AVG-204: findByTrialAndUserId query added

--- backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/usertrials/UserTrialRepository.java
+++ backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/usertrials/UserTrialRepository.java
@@ -1,11 +1,15 @@
 package eu.mostserene.avogador.exerciseservice.usertrials;
 
+import eu.mostserene.avogador.exerciseservice.trials.Trial;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserTrialRepository extends JpaRepository<UserTrial, UUID> {
+    Optional<UserTrial> findByTrialAndUserId(@NonNull Trial trial, @NonNull UUID userId);
     List<UserTrial> findByUserId(UUID userId);
     List<UserTrial> findByTrial_Id(UUID id);
 }
