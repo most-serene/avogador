e42e3fd7544f8bb7853253d1b1b74fb451adf8a5
Alessio-Campa <ale.campa00@gmail.com>
2023-09-20 12:47:15 +0200

    AVG-196 Added TrialServiceImpl

--- backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/trials/TrialServiceImpl.java
+++ backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/trials/TrialServiceImpl.java
@@ -1,2 +1,22 @@
+<<<<<<< Updated upstream
 package eu.mostserene.avogador.exerciseservice.trials;public class TrialServiceImpl {
+=======
+package eu.mostserene.avogador.exerciseservice.trials;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class TrialServiceImpl implements TrialService {
+    @Autowired
+    private TrialRepository repository;
+
+    @Override
+    public Optional<Trial> getTrialById(UUID trialId) {
+        return repository.findById(trialId);
+    }
+>>>>>>> Stashed changes
 }
