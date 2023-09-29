b397291a5a3ccf0cf9a8806b16c956f1a24a5b2e
Mario Gottardo <mario.gottardo27@gmail.com>
2023-09-29 09:47:50 +0200

    AVG-238: fixed speed dial permission

--- frontend/src/components/home/HomeScreen.tsx
+++ frontend/src/components/home/HomeScreen.tsx
@@ -1,23 +1,39 @@
 import CoursesPreview from "@home/CoursesPreview.tsx";
 import Grid from "@mui/material/Grid";
 import DeadlineStack from "@home/DeadlineStack.tsx";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAtom } from "jotai";
 import userAtom from "@authentication/userAtom.ts";
 import { Skeleton, Typography } from "@mui/material";
+import useCourseService from "@courses/hooks/useCourseService.tsx";
+import { UserCourse } from "@courses/types.ts";
+import { enqueueSnackbar } from "notistack";
 import QuickCreationHome from "@home/QuickCreationHome.tsx";
 
 export default function HomeScreen() {
   const [user] = useAtom(userAtom);
   const navigate = useNavigate();
+  const { getUserCourses } = useCourseService();
+  const [userCourses, setUserCourses] = useState<UserCourse[]>();
 
   useEffect(() => {
-    console.log(user);
     if (user === null) {
       navigate("/login");
+      return;
     }
-  }, [user, navigate]);
+    if (user === undefined) {
+      return;
+    }
+
+    getUserCourses(user.id)
+      .then((resUserCourses) => {
+        setUserCourses(resUserCourses);
+      })
+      .catch((err: Error) =>
+        enqueueSnackbar(err.message, { variant: "error" }),
+      );
+  }, [user, navigate, getUserCourses]);
 
   return (
     <>
@@ -26,8 +42,8 @@ export default function HomeScreen() {
           <Typography variant={"h4"} marginY={2}>
             {user ? `Welcome back ${user.givenName}!` : <Skeleton />}
           </Typography>
-          <CoursesPreview />
-          <QuickCreationHome />
+          <CoursesPreview courses={userCourses?.map((elem) => elem.course)} />
+          {userCourses && <QuickCreationHome userCourses={userCourses} />}
         </Grid>
 
         <Grid item xs={3} paddingLeft={1}>
