2849ac13f359402d3a9c0f9db7ce8d5f9d62f7e7
Alessio-Campa <ale.campa00@gmail.com>
2023-10-02 15:01:03 +0200

    AVG-258 Added SubmissionResult Entity class

---
+++ backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/submissionresults/SubmissionStatus.java
@@ -0,0 +1,10 @@
+package eu.mostserene.avogador.exerciseservice.submissionresults;
+
+public enum SubmissionStatus {
+    PENDING,
+    CORRECT,
+    WRONG_ANSWER,
+    TIME_LIMIT_EXCEEDED,
+    RUNTIME_ERROR,
+    COMPILE_ERROR,
+}
