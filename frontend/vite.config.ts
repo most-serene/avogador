64b7e874a137bbb6bc37c6da8fc218f79cc00e72
Mario Gottardo <mario.gottardo27@gmail.com>
2023-10-04 16:30:03 +0200

    CICD and Vite build hotfixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -69,15 +69,4 @@ export default defineConfig({
   optimizeDeps: {
     include: ["@emotion/react", "@emotion/styled"],
   },
-  build: {
-    rollupOptions: {
-      output:{
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    }
-  }
 })
