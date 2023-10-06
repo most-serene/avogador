48e101303b0274a856ffed09d26f2272a94b1dc4
Mario Gottardo <mario.gottardo27@gmail.com>
2023-10-06 09:44:20 +0200

    AVG-266: trial screen skeleton

---
+++ frontend/src/components/trials/trialDetail/TrialDetailStudentScreen.tsx
@@ -0,0 +1,50 @@
+import { Box, Button, Typography } from "@mui/material";
+import Container from "@mui/material/Container";
+import { Exam, Practice } from "@trials/types.ts";
+import { CourseDetail } from "@courses/types.ts";
+import { useNavigate } from "react-router-dom";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+
+interface TrialDetailStudentScreenProps {
+  trial: Practice | Exam;
+  course: CourseDetail;
+}
+
+const TrialDetailStudentScreen = ({
+  trial,
+  course,
+}: TrialDetailStudentScreenProps) => {
+  const navigate = useNavigate();
+
+  return (
+    <Box
+      height="100%"
+      sx={{
+        flexGrow: 1,
+        display: "flex",
+      }}
+    >
+      <Container maxWidth={false}>
+        <Box display={"flex"} justifyContent={"center"}>
+          <Typography variant="h3" align="center">
+            {trial.name}
+          </Typography>
+          <Box style={{ position: "absolute", left: "2rem", top: "5rem" }}>
+            <Button
+              variant={"outlined"}
+              onClick={() => {
+                navigate(`/courses/${course.id}?tab=1`);
+              }}
+            >
+              <ArrowBackIosNewIcon />
+              Back to {course.name}
+            </Button>
+          </Box>
+        </Box>
+        <>Trial exercises tab</>
+      </Container>
+    </Box>
+  );
+};
+
+export default TrialDetailStudentScreen;
