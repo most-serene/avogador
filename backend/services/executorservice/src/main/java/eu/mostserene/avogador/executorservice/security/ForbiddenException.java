392fa8f24bb4e2dbebfd5f64b19a96c9aa77806e
Mario Gottardo <mario.gottardo27@gmail.com>
2023-10-09 11:22:11 +0200

    AVG-274: security package imported

---
+++ backend/services/executorservice/src/main/java/eu/mostserene/avogador/executorservice/security/ForbiddenException.java
@@ -0,0 +1,22 @@
+package eu.mostserene.avogador.executorservice.security;
+
+import eu.mostserene.avogador.executorservice.users.UserDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.FORBIDDEN, reason = "Forbidden")
+@Slf4j
+public class ForbiddenException extends RuntimeException{
+    public ForbiddenException(UserDto user) {
+        super("Forbidden");
+        /*log.info(LoggerColors.error("Forbidden access caught from " + user.getEmail() +
+                " (" + user.getGivenName() + " " + user.getFamilyName() + ") "));*/
+        log.info("Forbidden access caught from " + user.getEmail() +
+                " (" + user.getGivenName() + " " + user.getFamilyName() + ") ");
+    }
+
+    public ForbiddenException(String message) {
+        super(message);
+    }
+}
---
+++ backend/services/executorservice/src/main/java/eu/mostserene/avogador/executorservice/security/restapicontrol/RequestSource.java
@@ -0,0 +1,21 @@
+package eu.mostserene.avogador.executorservice.security.restapicontrol;
+
+public enum RequestSource {
+    REACT_APP("React-App"),
+    REST_API("Rest-Api");
+
+    public final String label;
+
+    public static RequestSource valueOfLabel(String label) {
+        for (RequestSource source : values()) {
+            if (source.label.equals(label)) {
+                return source;
+            }
+        }
+        return null;
+    }
+
+    private RequestSource(String label) {
+        this.label = label;
+    }
+}
