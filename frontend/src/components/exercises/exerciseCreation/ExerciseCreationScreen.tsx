10420c87d5e3b81141ced8538342fcd5303a282a
Alessio Campanelli <64778464+Alessio-Campa@users.noreply.github.com>
2023-10-11 11:20:06 +0200

    AVG-270 exercise creation testcases (#209)
    * AVG-270 Exercise creation - testcases step
    
    * AVG-270 Added submission handling
    
    Template submission is still missing. It will be implemented once the relative enpoint is created
    
    * AVG-270 Added Testcase visibility setting
    
    * AVG-270 minor fixes

--- frontend/src/components/exercises/exerciseCreation/ExerciseCreationScreen.tsx
+++ frontend/src/components/exercises/exerciseCreation/ExerciseCreationScreen.tsx
@@ -1,6 +1,14 @@
-import { Button, Step, StepLabel, Stepper, Typography } from "@mui/material";
+import {
+  Backdrop,
+  Button,
+  CircularProgress,
+  Step,
+  StepLabel,
+  Stepper,
+  Typography,
+} from "@mui/material";
 import ExerciseCreationInfo from "@exercises/exerciseCreation/ExerciseCreationInfo.tsx";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Box from "@mui/material/Box";
 import { useAtom } from "jotai";
 import exerciseAtom, {
@@ -10,63 +18,191 @@ import ExerciseCreationTemplate from "@exercises/exerciseCreation/template/Exerc
 import templateAtom, {
   getInitializedTemplate,
 } from "@exercises/exerciseCreation/TemplateAtom.ts";
+import ExerciseCreationTestcases from "@exercises/exerciseCreation/testcases/ExerciseCreationTestcases.tsx";
+import testcasesAtom from "@exercises/exerciseCreation/TestcasesAtom.ts";
+import useExerciseService from "@exercises/hooks/useExerciseService.tsx";
+import { enqueueSnackbar } from "notistack";
+import { Exercise, Testcase } from "@exercises/types.ts";
+import { useNavigate } from "react-router-dom";
 
 const steps = [
   { label: "General Info", component: <ExerciseCreationInfo /> },
   { label: "Template", component: <ExerciseCreationTemplate /> },
-  { label: "Testcases", component: <Typography>Testcases</Typography> },
+  { label: "Testcases", component: <ExerciseCreationTestcases /> },
 ];
 
 const ExerciseCreationScreen = () => {
+  const { createExercise, createTestcase } = useExerciseService();
+  const navigate = useNavigate();
   const [activeStep, setActiveStep] = useState(0);
-  const [, setExercise] = useAtom(exerciseAtom);
-  const [, setTemplate] = useAtom(templateAtom);
+  const [exercise, setExercise] = useAtom(exerciseAtom);
+  const [template, setTemplate] = useAtom(templateAtom);
+  const [testcases, setTestcases] = useAtom(testcasesAtom);
+  const [creationStatus, setCreationStatus] = useState("");
+  const [creationPercentage, setCreationPercentage] = useState(0);
+
+  const isInformationStepComplete = useMemo<boolean>(() => {
+    return (
+      exercise.courseId !== "" &&
+      exercise.trialId !== "" &&
+      exercise.name.trim() !== "" &&
+      exercise.statement.trim() !== "" &&
+      exercise.timeLimit > 0
+    );
+  }, [exercise]);
+
+  const isTemplateStepComplete = useMemo<boolean>(() => {
+    return template.some((cell) => cell.type === "EDITABLE");
+  }, [template]);
+
+  const isTestcasesStepComplete = useMemo<boolean>(() => {
+    return (
+      testcases.length > 0 &&
+      testcases.every(
+        ({ input, output }) => input.trim() !== "" && output.trim() !== "",
+      )
+    );
+  }, [testcases]);
 
   useEffect(() => {
     setExercise(getInitializedExercise());
     setTemplate([...getInitializedTemplate()]);
+    setTestcases([]);
 
     return () => {
       setExercise(getInitializedExercise());
       setTemplate([...getInitializedTemplate()]);
+      setTestcases([]);
     };
-  }, [setExercise, setTemplate]);
+  }, [setExercise, setTemplate, setTestcases]);
+
+  const handleSubmit = async () => {
+    let createdExercise: Exercise;
+    const createdTestcases: Testcase[] = [];
+
+    try {
+      setCreationStatus("Creating the Database entry");
+      createdExercise = await createExercise({
+        trialId: exercise.trialId,
+        name: exercise.name,
+        statement: exercise.statement,
+        timeLimit: exercise.timeLimit,
+        isVisible: exercise.isVisible,
+      });
+      setCreationPercentage(creationPercentage + 25);
+    } catch (err) {
+      if (err instanceof Error) {
+        enqueueSnackbar(err.message, { variant: "error" });
+      }
+      setCreationStatus("");
+      setCreationPercentage(0);
+      return;
+    }
+
+    // create template
+    setCreationPercentage(creationPercentage + 25);
+
+    let i = 1;
+    for (const testcase of testcases) {
+      let createdTestcase: Testcase;
+      try {
+        setCreationStatus(
+          `Creating the Testcase entry ${i++}/${testcases.length}`,
+        );
+        createdTestcase = await createTestcase(createdExercise.id, testcase);
+        setCreationPercentage(
+          creationPercentage + ((i - 1) / testcases.length) * 50,
+        );
+      } catch (err) {
+        if (err instanceof Error) {
+          enqueueSnackbar(err.message, { variant: "error" });
+        }
+        setCreationStatus("");
+        setCreationPercentage(0);
+        return;
+      }
+      createdTestcases.push(createdTestcase);
+    }
+
+    if (createdTestcases.length === testcases.length) {
+      setCreationStatus("");
+      navigate(`/exercises`);
+    }
+  };
 
   return (
-    <Box height="100%">
-      <Stepper activeStep={activeStep} alternativeLabel>
-        {steps.map(({ label }) => (
-          <Step key={label}>
-            <StepLabel>{label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-      <Box marginY={2} style={{ height: "calc(100% - 150px)" }}>
-        {steps[activeStep].component}
+    <>
+      <Box height="100%">
+        <Stepper activeStep={activeStep} alternativeLabel>
+          {steps.map(({ label }) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+        <Box marginY={2} style={{ height: "calc(100% - 150px)" }}>
+          {steps[activeStep].component}
+        </Box>
+        <Box display="flex" justifyContent="center">
+          <Button
+            variant="outlined"
+            sx={{ mx: 1 }}
+            disabled={activeStep <= 0}
+            onClick={() => {
+              setActiveStep(Math.max(activeStep - 1, 0));
+            }}
+          >
+            Go back
+          </Button>
+          <Button
+            variant="outlined"
+            sx={{
+              mx: 1,
+              display: activeStep >= steps.length - 1 ? "none" : "block",
+            }}
+            disabled={
+              activeStep >= steps.length - 1 ||
+              (activeStep === 0 && !isInformationStepComplete) ||
+              (activeStep === 1 && !isTemplateStepComplete)
+            }
+            onClick={() => {
+              setActiveStep(Math.min(activeStep + 1, steps.length - 1));
+            }}
+          >
+            Next
+          </Button>
+          <Button
+            variant="outlined"
+            sx={{
+              mx: 1,
+              display: activeStep < steps.length - 1 ? "none" : "block",
+            }}
+            disabled={
+              activeStep != steps.length - 1 || !isTestcasesStepComplete
+            }
+            onClick={() => void handleSubmit()}
+          >
+            Create
+          </Button>
+        </Box>
       </Box>
-      <Box display="flex" justifyContent="center">
-        <Button
-          variant="outlined"
-          sx={{ mx: 1 }}
-          disabled={activeStep <= 0}
-          onClick={() => {
-            setActiveStep(Math.max(activeStep - 1, 0));
-          }}
-        >
-          Go back
-        </Button>
-        <Button
-          variant="outlined"
-          sx={{ mx: 1 }}
-          disabled={activeStep >= steps.length - 1}
-          onClick={() => {
-            setActiveStep(Math.min(activeStep + 1, steps.length - 1));
-          }}
-        >
-          Next
-        </Button>
-      </Box>
-    </Box>
+      <Backdrop
+        sx={{
+          zIndex: (theme) => theme.zIndex.drawer + 1,
+        }}
+        open={creationStatus !== ""}
+      >
+        <Box display="flex" flexDirection="column" alignItems="center">
+          <CircularProgress
+            variant="determinate"
+            value={creationPercentage}
+            color="primary"
+            sx={{ mb: 2 }}
+          />
+          <Typography>{creationStatus}</Typography>
+        </Box>
+      </Backdrop>
+    </>
   );
 };
 
