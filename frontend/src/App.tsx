e550ac7a6fa7f808b232b20e33ac38cdbf1442fc
Mario Gottardo <58598676+Gotti27@users.noreply.github.com>
2023-10-15 10:54:25 +0200

    WebSocket enhanced (#223)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,7 +22,7 @@ import MobileWrapper from "@structure/MobileWrapper/MobileWrapper";
 import TrialCreationScreen from "@trials/trialCreation/TrialCreationScreen.tsx";
 import ExerciseCreationScreen from "@exercises/exerciseCreation/ExerciseCreationScreen.tsx";
 import TrialDetailScreen from "@trials/trialDetail/TrialDetailScreen.tsx";
-import useWebSocket from "@hooks/useWebSocket.tsx";
+import WebSocketWrapper from "./WebSocketWrapper.tsx";
 
 const NotFound = () => {
   return (
@@ -53,17 +53,6 @@ function App() {
   const { connectToGlitchTip } = useGlitchTip();
   const [user] = useAtom(userAtom);
 
-  const { client } = useWebSocket();
-
-  useEffect(() => {
-    if (client.connected) {
-      console.log("subscribing");
-      client.subscribe("/broadcast", (message) => {
-        console.log(message);
-      });
-    }
-  }, [client, client.connected]);
-
   useEffect(() => {
     if (user) {
       connectToGlitchTip(user);
@@ -85,75 +74,77 @@ function App() {
           <Navbar ref={navbarRef} />
           <ErrorHandlerWrapper>
             <AuthWrapper>
-              <MobileWrapper>
-                <Box
-                  id="fullScreenWrapper"
-                  height={`calc(100vh - ${occupiedHeight}px)`}
-                >
-                  <Routes>
-                    <Route
-                      path="/"
-                      element={
-                        <Container maxWidth={false} sx={{ height: "100%" }}>
-                          <HomeScreen />
-                        </Container>
-                      }
-                    />
-                    <Route
-                      path="/status"
-                      element={
-                        <Container>
-                          <StatusPage />
-                        </Container>
-                      }
-                    />
-                    <Route path="courses">
+              <WebSocketWrapper>
+                <MobileWrapper>
+                  <Box
+                    id="fullScreenWrapper"
+                    height={`calc(100vh - ${occupiedHeight}px)`}
+                  >
+                    <Routes>
                       <Route
-                        path={""}
+                        path="/"
                         element={
-                          <Container maxWidth={"xl"}>
-                            <CoursesScreen />
+                          <Container maxWidth={false} sx={{ height: "100%" }}>
+                            <HomeScreen />
                           </Container>
                         }
                       />
                       <Route
-                        path={"new"}
+                        path="/status"
                         element={
-                          <Container maxWidth={"xl"}>
-                            <CourseCreationScreen />
+                          <Container>
+                            <StatusPage />
                           </Container>
                         }
                       />
+                      <Route path="courses">
+                        <Route
+                          path={""}
+                          element={
+                            <Container maxWidth={"xl"}>
+                              <CoursesScreen />
+                            </Container>
+                          }
+                        />
+                        <Route
+                          path={"new"}
+                          element={
+                            <Container maxWidth={"xl"}>
+                              <CourseCreationScreen />
+                            </Container>
+                          }
+                        />
+                        <Route
+                          path={":courseId"}
+                          element={<CourseDetailScreen />}
+                        />
+                        <Route
+                          path={":courseId/join"}
+                          element={<JoinCourseScreen />}
+                        />
+                      </Route>
                       <Route
-                        path={":courseId"}
-                        element={<CourseDetailScreen />}
+                        path={"/trials/new"}
+                        element={<TrialCreationScreen />}
                       />
                       <Route
-                        path={":courseId/join"}
-                        element={<JoinCourseScreen />}
+                        path={"/practices/:trialId"}
+                        element={<TrialDetailScreen trialType={"PRACTICE"} />}
                       />
-                    </Route>
-                    <Route
-                      path={"/trials/new"}
-                      element={<TrialCreationScreen />}
-                    />
-                    <Route
-                      path={"/practices/:trialId"}
-                      element={<TrialDetailScreen trialType={"PRACTICE"} />}
-                    />
-                    <Route path="/profile" element={<ProfileScreen />} />
-                    <Route
-                      path="/exercises/new"
-                      element={
-                        <Container maxWidth={"xl"} style={{ height: "100%" }}>
-                          <ExerciseCreationScreen />
-                        </Container>
-                      }
-                    />
-                    <Route path="*" element={<NotFound />} />
-                  </Routes>
-                </Box>
-              </MobileWrapper>
+                      <Route path="/profile" element={<ProfileScreen />} />
+                      <Route
+                        path="/exercises/new"
+                        element={
+                          <Container maxWidth={"xl"} style={{ height: "100%" }}>
+                            <ExerciseCreationScreen />
+                          </Container>
+                        }
+                      />
+                      <Route path="*" element={<NotFound />} />
+                    </Routes>
+                  </Box>
+                </MobileWrapper>
+              </WebSocketWrapper>
             </AuthWrapper>
           </ErrorHandlerWrapper>
           <Footer ref={footerRef} />
--- frontend/src/hooks/useWebSocket.tsx
+++ frontend/src/hooks/useWebSocket.tsx
@@ -1,26 +1,58 @@
-import { useMemo } from "react";
-import { Client } from "@stomp/stompjs";
+import { Client, Message, StompSubscription } from "@stomp/stompjs";
+import { atom, useAtom } from "jotai";
+import { useCallback, useEffect } from "react";
+
+const isSocketConnectedAtom = atom(false);
+const socketClientAtom = atom<Client>(() => {
+  const client = new Client({
+    brokerURL: `${import.meta.env.VITE_AVOGADOR_BACKEND_WEBSOCKET_ADDRESS}`,
+    onConnect: (frame) => {
+      console.log(frame);
+    },
+    onStompError: (frame) => {
+      console.log(frame);
+    },
+    onWebSocketError: (e) => {
+      console.log(e);
+    },
+  });
+  client.activate();
+  return client;
+});
 
 const useWebSocket = () => {
-  const client = useMemo(() => {
-    const client = new Client({
-      brokerURL: `${import.meta.env.VITE_AVOGADOR_BACKEND_WEBSOCKET_ADDRESS}`,
-      onConnect: (frame) => {
-        console.log(frame);
-      },
-      onStompError: (frame) => {
-        console.log(frame);
-      },
-      onWebSocketError: (e) => {
-        console.log(e);
-      },
-    });
-    client.activate();
-    return client;
-  }, []);
+  const [socketClient] = useAtom(socketClientAtom);
+  const [isSocketConnected, setIsSocketConnected] = useAtom(
+    isSocketConnectedAtom,
+  );
+
+  useEffect(() => {
+    socketClient.onConnect = (frame) => {
+      console.log(frame);
+      setIsSocketConnected(true);
+    };
+  }, [setIsSocketConnected, socketClient]);
+
+  const subscribe: (
+    topic: string,
+    cb: (message: Message) => void,
+  ) => Promise<StompSubscription> = useCallback(
+    (topic: string, cb: (message: Message) => void) => {
+      return new Promise<StompSubscription>((resolve, reject) => {
+        if (isSocketConnected) {
+          resolve(socketClient.subscribe(topic, cb));
+        } else {
+          reject("Socket not connected");
+        }
+      });
+    },
+    [isSocketConnected, socketClient],
+  );
 
   return {
-    client,
+    isSocketConnected,
+    socketClient,
+    subscribe,
   };
 };
 
