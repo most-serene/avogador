32c3bb746ecaf7c1875f9d7d4d315c390448a629
Mario Gottardo <mario.gottardo27@gmail.com>
2023-10-22 11:12:22 +0200

    AVG-304: change trial visibility

--- frontend/src/components/trials/TrialItem/TrialItem.tsx
+++ frontend/src/components/trials/TrialItem/TrialItem.tsx
@@ -2,17 +2,62 @@ import { Card, CardContent, Typography } from "@mui/material";
 import { isExam, isPractice, Trial } from "@trials/types.ts";
 import PracticeItem from "@trials/TrialItem/PracticeItem.tsx";
 import ExamItem from "@trials/TrialItem/ExamItem.tsx";
+import ContextMenuWrapper from "@structure/ContextMenuWrapper/ContextMenuWrapper.tsx";
+import MenuItem from "@mui/material/MenuItem";
+import { enqueueSnackbar } from "notistack";
+import useTrialService from "@trials/hooks/useTrialService.tsx";
+import { useState } from "react";
 
 interface TrialItemProps {
   trial: Trial;
 }
 
 const TrialItem = ({ trial }: TrialItemProps) => {
-  if (isPractice(trial)) {
-    return <PracticeItem practice={trial} />;
+  const { updatePractice } = useTrialService();
+  const [trialState, setTrialState] = useState(trial);
+
+  if (isPractice(trialState)) {
+    return (
+      <ContextMenuWrapper
+        menu={
+          <>
+            <MenuItem
+              disabled={trialState.isVisible}
+              onClick={() => {
+                updatePractice({ ...trialState, isVisible: true })
+                  .then((updatedTrial) => {
+                    setTrialState(updatedTrial);
+                  })
+                  .catch((err: Error) => {
+                    enqueueSnackbar(err.message, { variant: "error" });
+                  });
+              }}
+            >
+              Turn visible
+            </MenuItem>
+            <MenuItem
+              disabled={!trialState.isVisible}
+              onClick={() => {
+                updatePractice({ ...trialState, isVisible: false })
+                  .then((updatedTrial) => {
+                    setTrialState(updatedTrial);
+                  })
+                  .catch((err: Error) => {
+                    enqueueSnackbar(err.message, { variant: "error" });
+                  });
+              }}
+            >
+              Turn hidden
+            </MenuItem>
+          </>
+        }
+      >
+        <PracticeItem practice={trialState} />
+      </ContextMenuWrapper>
+    );
   }
-  if (isExam(trial)) {
-    return <ExamItem exam={trial} />;
+  if (isExam(trialState)) {
+    return <ExamItem exam={trialState} />;
   }
 
   return (
