6627808405dafd8cba50b973dc24ff2c4576820d
Mario Gottardo <58598676+Gotti27@users.noreply.github.com>
2023-10-26 12:05:25 +0200

    AVG-309: sync trial kanban (#242)

--- frontend/src/components/courses/courseDetail/CourseTrialsTab/CourseTrialsTab.tsx
+++ frontend/src/components/courses/courseDetail/CourseTrialsTab/CourseTrialsTab.tsx
@@ -1,9 +1,9 @@
 import Grid from "@mui/material/Grid";
 import KanbanColumn from "@courses/courseDetail/CourseTrialsTab/KanbanColumn.tsx";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import useTrialService from "@trials/hooks/useTrialService.tsx";
 import { UserCourseDetail } from "@courses/types.ts";
-import { Trial } from "@trials/types.ts";
+import { isPractice, Trial } from "@trials/types.ts";
 import { enqueueSnackbar } from "notistack";
 
 interface CourseTrialsTabProps {
@@ -21,30 +21,65 @@ const CourseTrialsTab = ({ userCourse }: CourseTrialsTabProps) => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    const timeouts: NodeJS.Timeout[] = [];
     if (userCourse == null) {
       return;
     }
     getTrialsByCourseId(userCourse.id)
       .then((trials) => {
         setTrials(trials);
+
+        const addTimeout = (time: number) => {
+          timeouts.push(
+            setTimeout(() => {
+              setTrials([...trials]);
+            }, time - Date.now()),
+          );
+        };
+
+        trials
+          .filter((trial) => isTrialScheduled(trial) || isTrialOngoing(trial))
+          .map((trial) => {
+            if (isPractice(trial)) {
+              addTimeout(trial.deadline.getTime());
+            }
+            return trial;
+          })
+          .filter((trial) => isTrialScheduled(trial))
+          .map((trial) => {
+            addTimeout(trial.startTimestamp.getTime());
+          });
+
         setIsLoading(false);
       })
       .catch((err: Error) => {
-        enqueueSnackbar(err.name, { variant: "error" });
+        enqueueSnackbar(err.message, { variant: "error" });
       });
-  }, [getTrialsByCourseId, userCourse]);
 
-  const scheduledTrials = trials
-    .filter((trial) => isTrialScheduled(trial))
-    .sort((a, b) => a.startTimestamp.getTime() - b.startTimestamp.getTime());
+    return () => {
+      timeouts.forEach((timeout) => {
+        clearTimeout(timeout);
+      });
+    };
+  }, [getTrialsByCourseId, isTrialOngoing, isTrialScheduled, userCourse]);
 
-  const ongoingTrials = trials
-    .filter((trial) => isTrialOngoing(trial))
-    .sort((a, b) => a.startTimestamp.getTime() - b.startTimestamp.getTime());
+  const scheduledTrials = useMemo(() => {
+    return trials
+      .filter((trial) => isTrialScheduled(trial))
+      .sort((a, b) => a.startTimestamp.getTime() - b.startTimestamp.getTime());
+  }, [isTrialScheduled, trials]);
 
-  const endedTrials = trials
-    .filter((trial) => isTrialEnded(trial))
-    .sort((a, b) => b.startTimestamp.getTime() - a.startTimestamp.getTime());
+  const ongoingTrials = useMemo(() => {
+    return trials
+      .filter((trial) => isTrialOngoing(trial))
+      .sort((a, b) => a.startTimestamp.getTime() - b.startTimestamp.getTime());
+  }, [isTrialOngoing, trials]);
+
+  const endedTrials = useMemo(() => {
+    return trials
+      .filter((trial) => isTrialEnded(trial))
+      .sort((a, b) => b.startTimestamp.getTime() - a.startTimestamp.getTime());
+  }, [isTrialEnded, trials]);
 
   return (
     <Grid container height="100%" width={"100%"} spacing={2}>
