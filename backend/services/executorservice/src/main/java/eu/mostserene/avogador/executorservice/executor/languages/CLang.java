882b6bfa6ddb55108e0cee4fd2960f2fdc8d8cd3
Mario Gottardo <mario.gottardo27@gmail.com>
2023-11-23 19:29:20 +0100

    AVG-344: Languages code cleanup

--- backend/services/executorservice/src/main/java/eu/mostserene/avogador/executorservice/executor/languages/CLang.java
+++ backend/services/executorservice/src/main/java/eu/mostserene/avogador/executorservice/executor/languages/CLang.java
@@ -3,9 +3,8 @@ package eu.mostserene.avogador.executorservice.executor.languages;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.async.ResultCallback;
 import com.github.dockerjava.api.command.CreateContainerResponse;
-import com.github.dockerjava.api.model.Frame;
 import com.github.dockerjava.api.model.Statistics;
-import com.github.dockerjava.api.model.WaitResponse;
+import eu.mostserene.avogador.executorservice.executor.SandboxesUtils;
 import eu.mostserene.avogador.executorservice.executor.TLEDetector;
 import eu.mostserene.avogador.executorservice.submission.Submission;
 import eu.mostserene.avogador.executorservice.utils.LoggerColors;
@@ -15,11 +14,9 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.rauschig.jarchivelib.Archiver;
 import org.rauschig.jarchivelib.ArchiverFactory;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
 @Slf4j
@@ -38,107 +35,42 @@ public class CLang implements Language {
     public Pair<File, String> compile(DockerClient dockerClient, File sourceCode) {
         log.info(LoggerColors.warn("Compiling c: " + sourceCode));
         CreateContainerResponse compilerDocker = dockerClient.createContainerCmd("gotti27/runtime-env:stable")
-                .withCmd("gcc", "-o", "/execution/program", "/" + sourceCode.getName()) //, "-lstdc++")
+                .withCmd("gcc", "-o", "/execution/program", "/" + sourceCode.getName())
                 .withNetworkDisabled(true)
                 .exec();
 
         dockerClient.copyArchiveToContainerCmd(compilerDocker.getId())
-                //.withHostResource((new File(sourceCode.getParentFile() + "/code")).toPath().toString())
                 .withHostResource(sourceCode.toPath().toString())
                 .withRemotePath("/")
                 .exec();
 
         dockerClient.startContainerCmd(compilerDocker.getId()).exec();
-        ByteArrayOutputStream compilerOutputStream = new ByteArrayOutputStream();
         try {
-            TLEDetector compileTLDetector = new TLEDetector();
-
-            dockerClient.statsCmd(compilerDocker.getId()).exec(new ResultCallback.Adapter<>() {
-                @Override
-                public void onNext(Statistics stats) {
-                    super.onNext(stats);
-
-                    if (Objects.requireNonNull(Objects.requireNonNull(stats.getCpuStats().getCpuUsage())
-                            .getTotalUsage()) / 1000000000L >= 60) {
-                        dockerClient.stopContainerCmd(compilerDocker.getId()).withTimeout(0).exec();
-
-                        compileTLDetector.detect();
-                        onComplete();
-                    }
-                    if (Boolean.FALSE.equals(dockerClient.inspectContainerCmd(compilerDocker.getId()).exec().getState().getRunning())) {
-                        onComplete();
-                    }
-                }
-
-                @Override
-                public void onComplete() {
-                    super.onComplete();
-                }
-            }).awaitCompletion();
-
-            dockerClient.waitContainerCmd(compilerDocker.getId()).exec(new ResultCallback.Adapter<>() {
-                @Override
-                public void onNext(WaitResponse object) {
-                    super.onNext(object);
-                }
-            }).awaitCompletion();
-
-            dockerClient.logContainerCmd(compilerDocker.getId())
-                    .withStdOut(true)
-                    .withStdErr(true)
-                    .withFollowStream(false)
-                    .exec(new ResultCallback.Adapter<>() {
-                        @Override
-                        public void onNext(Frame object) {
-                            super.onNext(object);
-                            try {
-                                compilerOutputStream.write(object.getPayload());
-                            } catch (IOException e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-                    }).awaitCompletion();
-
-            if (compileTLDetector.wasDetected()) {
-                dockerClient.removeContainerCmd(compilerDocker.getId()).exec();
-                return Pair.of(null, "Compile time exceeded");
-            }
-
-            InputStream inputStream = dockerClient.copyArchiveFromContainerCmd(compilerDocker.getId(), "/execution")
-                    .exec();
-
-            File target = new File(sourceCode.getParentFile() + "/program.tar");
-            FileUtils.copyInputStreamToFile(inputStream, target);
-
-            Archiver archiver = ArchiverFactory.createArchiver("tar");
-            archiver.extract(target, new File(target.getParentFile() + "/program"));
-
-            dockerClient.removeContainerCmd(compilerDocker.getId()).exec();
+            return runCompilerSandbox(dockerClient, compilerDocker.getId(), sourceCode);
         } catch (InterruptedException | IOException e) {
             log.error(LoggerColors.error("compilation failed"));
             throw new RuntimeException(e);
+        } finally {
+            dockerClient.removeContainerCmd(compilerDocker.getId()).exec();
         }
-
-        return Pair.of( new File(sourceCode.getParent() + "/program/execution"), compilerOutputStream.toString(StandardCharsets.UTF_8));
     }
+
 
     @Override
     public CreateContainerResponse configureExecutor(DockerClient dockerClient, File executable, File inputFile, Submission submission) {
         log.info(LoggerColors.cyan("Executing " + submission.getId()));
         var container = dockerClient.createContainerCmd("gotti27/runtime-env:stable").withImage("gotti27/runtime-env:stable")//.withUser("student")
-                .withCmd("/bin/bash", "-c", "chmod 777 /execution/program; timeout --foreground -k 0 -v " + submission.getTimeLimit() + " ./execution/program"  + " < /" + inputFile.getName())
+                .withCmd("/bin/bash", "-c", "chmod 777 /execution/program; timeout --foreground -k 0 -v " + submission.getTimeLimit() + " ./execution/program" + " < /" + inputFile.getName())
                 .withNetworkDisabled(true)
                 .exec();
 
         dockerClient.copyArchiveToContainerCmd(container.getId())
-                //.withHostResource((new File(sourceCode.getParentFile() + "/code")).toPath().toString())
                 .withHostResource(executable.toPath().toString())
                 .withCopyUIDGID(true)
                 .withRemotePath("/")
                 .exec();
 
         dockerClient.copyArchiveToContainerCmd(container.getId())
-                //.withHostResource((new File(sourceCode.getParentFile() + "/code")).toPath().toString())
                 .withHostResource(inputFile.toPath().toString())
                 .withCopyUIDGID(true)
                 .withRemotePath("/")
@@ -147,5 +79,56 @@ public class CLang implements Language {
         return container;
     }
 
+    private Pair<File, String> runCompilerSandbox(DockerClient dockerClient, String containerId, File sourceCode) throws InterruptedException, IOException {
+        TLEDetector compileTLDetector = new TLEDetector();
 
+        dockerClient.statsCmd(containerId)
+                .exec(getCompilationTimeLimitCallback(dockerClient, containerId, compileTLDetector))
+                .awaitCompletion();
+
+        SandboxesUtils.waitContainer(dockerClient, containerId);
+        String compilerOutputStream = SandboxesUtils.writeContainerLog(dockerClient, containerId, true, true);
+
+        if (compileTLDetector.wasDetected()) {
+            dockerClient.removeContainerCmd(containerId).exec();
+            return Pair.of(null, "Compile time exceeded");
+        }
+
+        InputStream inputStream = dockerClient.copyArchiveFromContainerCmd(containerId, "/execution")
+                .exec();
+
+        File target = new File(sourceCode.getParentFile() + "/program.tar");
+        FileUtils.copyInputStreamToFile(inputStream, target);
+
+        Archiver archiver = ArchiverFactory.createArchiver("tar");
+        archiver.extract(target, new File(target.getParentFile() + "/program"));
+
+        return Pair.of(new File(sourceCode.getParent() + "/program/execution"), compilerOutputStream);
+    }
+
+    private ResultCallback.Adapter<Statistics> getCompilationTimeLimitCallback(DockerClient dockerClient, String containerId, TLEDetector compileTLDetector) {
+
+        return new ResultCallback.Adapter<>() {
+            @Override
+            public void onNext(Statistics stats) {
+                super.onNext(stats);
+
+                if (Objects.requireNonNull(Objects.requireNonNull(stats.getCpuStats().getCpuUsage())
+                        .getTotalUsage()) / 1000000000L >= 60) {
+                    dockerClient.stopContainerCmd(containerId).withTimeout(0).exec();
+
+                    compileTLDetector.detect();
+                    onComplete();
+                }
+                if (Boolean.FALSE.equals(dockerClient.inspectContainerCmd(containerId).exec().getState().getRunning())) {
+                    onComplete();
+                }
+            }
+
+            @Override
+            public void onComplete() {
+                super.onComplete();
+            }
+        };
+    }
 }
