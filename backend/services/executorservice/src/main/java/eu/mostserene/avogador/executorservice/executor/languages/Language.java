1c8974c4495f4fc8b706e78cf17d25ce23815117
Mario Gottardo <58598676+Gotti27@users.noreply.github.com>
2023-11-23 22:44:49 +0100

    AVG-344: executor code cleanup (#278)

--- backend/services/executorservice/src/main/java/eu/mostserene/avogador/executorservice/executor/languages/Language.java
+++ backend/services/executorservice/src/main/java/eu/mostserene/avogador/executorservice/executor/languages/Language.java
@@ -1,7 +1,6 @@
 package eu.mostserene.avogador.executorservice.executor.languages;
 
 import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.command.CreateContainerCmd;
 import com.github.dockerjava.api.command.CreateContainerResponse;
 import eu.mostserene.avogador.executorservice.submission.Submission;
 import org.apache.commons.lang3.tuple.Pair;
@@ -10,11 +9,7 @@ import java.io.File;
 
 public interface Language {
     String getName();
-
     String getSupportedExtension();
-
     Pair<File, String> compile(DockerClient dockerClient, File sourceCode);
-
     CreateContainerResponse configureExecutor(DockerClient dockerClient, File executable, File inputFile, Submission submission);
-
 }
