fed861106dcd8c345229f11a7dc65634f08a726d
Mario Gottardo <58598676+Gotti27@users.noreply.github.com>
2023-11-24 09:01:29 +0100

    ApiGateway filter cleanup (#279)
    * ApiGateway filter cleanup
    
    * gateway fix - renamed foo

--- backend/apigateway/src/main/java/eu/mostserene/avogador/apigateway/security/CustomWebFilter.java
+++ backend/apigateway/src/main/java/eu/mostserene/avogador/apigateway/security/CustomWebFilter.java
@@ -3,6 +3,7 @@ package eu.mostserene.avogador.apigateway.security;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.hash.Hashing;
+import eu.mostserene.avogador.apigateway.utils.LoggerColors;
 import eu.mostserene.avogador.apigateway.utils.ProfileManager;
 import io.sentry.Sentry;
 import io.sentry.SentryLevel;
@@ -36,121 +37,18 @@ public class CustomWebFilter implements WebFilter {
     @Autowired
     private ProfileManager profileManager;
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     @NonNull
     @Order(5)
     public Mono<Void> filter(@NonNull ServerWebExchange exchange, @NonNull WebFilterChain chain) {
-        String uri = exchange.getRequest().getURI().getPath();
-        String requestId = RandomStringUtils.randomAlphanumeric(10);
-        String cookieName = getCookieName();
-
-        log.info("Request ID - " + requestId);
-
-        log.info("CSRF - " + isCSRF(exchange.getRequest()));
-
-        if (isCSRF(exchange.getRequest())) {
-            log.info(" -- CSRF detected?!");
-            ResponseCookie cookie = profileManager.executeOnProfile(
-                    this::developLogout,
-                    this::testingLogout,
-                    this::stagingLogout,
-                    this::productionLogout
-            );
-
-            exchange.getResponse().addCookie(cookie);
-            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-            return exchange.getResponse().setComplete();
-        }
-
-        log.info(uri);
-        log.info(String.valueOf(uri.matches("^/api.*")));
-        log.info(String.valueOf(!uri.matches("^/api/.*/api-key$")));
-
-        if (uri.matches("^/api.*") && !uri.matches("^/api/.*/api-key$")) {
-            if (exchange.getRequest().getCookies().getFirst(cookieName) != null) {
-                exchange.getResponse().setStatusCode(HttpStatus.BAD_REQUEST);
-                return exchange.getResponse().setComplete();
-            }
-            log.info("This is an API call");
-
-            String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-
-            if (authHeader == null || !"Bearer".equals(authHeader.split("\\s+")[0]) ||
-                    authHeader.split("\\s+")[1] == null) {
-                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-                return exchange.getResponse().setComplete();
-            }
-
-            String apiKey = authHeader.split("\\s+")[1];
-
-            Optional<AuthUserDTO> user = authService.decodeAndValidateApiKey(apiKey);
-
-            if (user.isPresent()) {
-                ObjectMapper objectMapper = new ObjectMapper();
-                try {
-                    return chain.filter(
-                            exchange.mutate().request(
-                                            exchange.getRequest().mutate()
-                                                    .header("User", objectMapper.writeValueAsString(user.get()))
-                                                    .header("Source", "Rest-Api")
-                                                    .header("Request-ID", requestId)
-                                                    .build())
-                                    .build());
-                } catch (JsonProcessingException e) {
-                    log.error(e.getMessage());
-                    exchange.getResponse().setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
-                    Sentry.captureException(e, scope -> scope.setLevel(SentryLevel.ERROR));
-                    return exchange.getResponse().setComplete();
-                }
-            } else {
-                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-                return exchange.getResponse().setComplete();
-            }
-        }
-
-        log.info("not api call");
-
-        if (uri.matches("^/|^/status$|^/users/google-auth.*|^/users/logout.*|^/users/current.*")) {
-            log.info("user auth call");
-            return chain.filter(
-                    exchange.mutate().request(
-                                    exchange.getRequest().mutate()
-                                            .header("Source", "React-App")
-                                            .header("Request-ID", requestId)
-                                            .build())
-                            .build());
-        }
-
-        HttpCookie cookie = exchange.getRequest().getCookies().getFirst(cookieName);
-
-        if (cookie == null) {
-            log.info("no JWT cookie");
-            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-            return exchange.getResponse().setComplete();
-        }
-
-        Optional<AuthUserDTO> user = authService.decodeAndValidateJWT(cookie.getValue());
-        log.info(user.toString());
-        if (user.isPresent()) {
-            ObjectMapper objectMapper = new ObjectMapper();
-            try {
-                log.info("Here's the user: " + user.get());
-                return chain.filter(
-                        exchange.mutate().request(
-                                        exchange.getRequest().mutate()
-                                                .header("User", objectMapper.writeValueAsString(user.get()))
-                                                .header("Source", "React-App")
-                                                .header("Request-ID", requestId)
-                                                .build())
-                                .build());
-            } catch (JsonProcessingException e) {
-                log.error(e.getMessage());
-                exchange.getResponse().setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
-                Sentry.captureException(e, scope -> scope.setLevel(SentryLevel.ERROR));
-                return exchange.getResponse().setComplete();
-            }
-        } else {
-            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        try {
+            return handleRequest(exchange, chain);
+        } catch (JsonProcessingException e) {
+            log.error(e.getMessage());
+            exchange.getResponse().setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+            Sentry.captureException(e, scope -> scope.setLevel(SentryLevel.ERROR));
             return exchange.getResponse().setComplete();
         }
     }
@@ -229,5 +127,112 @@ public class CustomWebFilter implements WebFilter {
         log.info(request.getHeaders().getFirst("Jwt-CSRF-Hash") + " " + jwtSubHash);
 
         return !jwtSubHash.equals(request.getHeaders().getFirst("Jwt-CSRF-Hash"));
+    }
+
+    private Mono<Void> handleCSRF(ServerWebExchange exchange) {
+        log.info(LoggerColors.warn(" -- CSRF detected?!"));
+        ResponseCookie cookie = profileManager.executeOnProfile(
+                this::developLogout,
+                this::testingLogout,
+                this::stagingLogout,
+                this::productionLogout
+        );
+
+        exchange.getResponse().addCookie(cookie);
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
+    }
+
+    private Mono<Void> handleRequest(ServerWebExchange exchange, WebFilterChain chain) throws JsonProcessingException {
+        String uri = exchange.getRequest().getURI().getPath();
+        String requestId = RandomStringUtils.randomAlphanumeric(10);
+        String cookieName = getCookieName();
+
+        if (isCSRF(exchange.getRequest())) {
+            return handleCSRF(exchange);
+        }
+
+        if (isApiCall(uri)) {
+            return handleApiCall(exchange, chain, requestId, cookieName);
+        }
+
+        if (isAuthenticationCall(uri)) {
+            return handleAuthenticationCall(exchange, chain, requestId);
+        }
+
+        HttpCookie cookie = exchange.getRequest().getCookies().getFirst(cookieName);
+
+        if (cookie == null) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        Optional<AuthUserDTO> user = authService.decodeAndValidateJWT(cookie.getValue());
+        if (user.isEmpty()) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return getSuccessfulChain(exchange, chain, user.get(), requestId);
+    }
+
+    private boolean isAuthenticationCall(String uri) {
+        return uri.matches("^/|^/status$|^/users/google-auth.*|^/users/logout.*|^/users/current.*|^/version/webapp");
+    }
+
+    private boolean isApiCall(String uri) {
+        return uri.matches("^/api.*") && !uri.matches("^/api/.*/api-key$");
+    }
+
+    private Mono<Void> getSuccessfulChain(ServerWebExchange exchange, WebFilterChain chain, AuthUserDTO user, String requestId) throws JsonProcessingException {
+        return chain.filter(
+                exchange.mutate().request(
+                                exchange.getRequest().mutate()
+                                        .header("User", objectMapper.writeValueAsString(user))
+                                        .header("Source", "React-App")
+                                        .header("Request-ID", requestId)
+                                        .build())
+                        .build());
+    }
+
+    private Mono<Void> handleAuthenticationCall(ServerWebExchange exchange, WebFilterChain chain, String requestId) {
+        return chain.filter(
+                exchange.mutate().request(
+                                exchange.getRequest().mutate()
+                                        .header("Source", "React-App")
+                                        .header("Request-ID", requestId)
+                                        .build())
+                        .build());
+    }
+
+    private Mono<Void> handleApiCall(ServerWebExchange exchange, WebFilterChain chain, String requestId, String cookieName) throws JsonProcessingException {
+        if (exchange.getRequest().getCookies().getFirst(cookieName) != null) {
+            exchange.getResponse().setStatusCode(HttpStatus.BAD_REQUEST);
+            return exchange.getResponse().setComplete();
+        }
+
+        String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        if (authHeader == null || !"Bearer".equals(authHeader.split("\\s+")[0]) ||
+                authHeader.split("\\s+")[1] == null) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        String apiKey = authHeader.split("\\s+")[1];
+        Optional<AuthUserDTO> user = authService.decodeAndValidateApiKey(apiKey);
+
+        if (user.isEmpty()) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(
+                exchange.mutate().request(
+                                exchange.getRequest().mutate()
+                                        .header("User", objectMapper.writeValueAsString(user.get()))
+                                        .header("Source", "Rest-Api")
+                                        .header("Request-ID", requestId)
+                                        .build())
+                        .build());
     }
 }
