2c9ad5c8154f7f42d760c2019c0b2b7db725b20d
Alessio-Campa <ale.campa00@gmail.com>
2023-12-21 10:32:51 +0100

    AVG-369 Added new markdown editor

---
+++ frontend/src/components/exercises/exerciseCreation/mardownEditors/OldMarkdownEditor.tsx
@@ -0,0 +1,35 @@
+import { useAtom } from "jotai";
+import exerciseAtom from "@exercises/exerciseCreation/ExerciseAtom.ts";
+import { Grid, TextField } from "@mui/material";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+const OldMarkdownEditor = () => {
+  const [exercise, setExercise] = useAtom(exerciseAtom);
+
+  return (
+    <>
+      <Grid item xs={6}>
+        <TextField
+          fullWidth
+          multiline
+          minRows={3}
+          label="Problem statement"
+          value={exercise.statement}
+          onChange={(event) => {
+            setExercise({ ...exercise, statement: event.target.value });
+          }}
+        />
+      </Grid>
+      <Grid item xs={6}>
+        <Markdown remarkPlugins={[remarkGfm]}>
+          {exercise.statement === ""
+            ? "_Start writing to see the markdown preview of the problem statement_"
+            : exercise.statement}
+        </Markdown>
+      </Grid>
+    </>
+  );
+};
+
+export default OldMarkdownEditor;
