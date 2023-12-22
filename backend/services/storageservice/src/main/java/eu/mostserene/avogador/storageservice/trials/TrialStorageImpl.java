b4d05c0536e01eba477317ac0d3bfb906021f789
Mario Gottardo <mario.gottardo27@gmail.com>
2023-12-22 16:01:55 +0100

    AVG-318: log storage methods

--- backend/services/storageservice/src/main/java/eu/mostserene/avogador/storageservice/trials/TrialStorageImpl.java
+++ backend/services/storageservice/src/main/java/eu/mostserene/avogador/storageservice/trials/TrialStorageImpl.java
@@ -1,14 +1,22 @@
 package eu.mostserene.avogador.storageservice.trials;
 
 import eu.mostserene.avogador.storageservice.FileSystemRoot;
+import eu.mostserene.avogador.storageservice.logger.AvogadorLogMessage;
 import eu.mostserene.avogador.storageservice.utils.FileCreationFailed;
 import eu.mostserene.avogador.storageservice.utils.LoggerColors;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.List;
 import java.util.UUID;
 
+@Getter
 @Slf4j
 public class TrialStorageImpl implements TrialStorage {
 
@@ -26,23 +34,18 @@ public class TrialStorageImpl implements TrialStorage {
         this.trialId = trialId;
     }
 
-    public UUID getCourseId() {
-        return courseId;
-    }
-
-    public UUID getTrialId() {
-        return trialId;
-    }
-
     public File getBaseDirectory() {
         return new File(fileSystemRoot.getFileSystemRoot() + "/courses/" +
                 getCourseId().toString() + "/trials/" );
     }
 
+    private File getTrialDirectory() {
+        return new File(getBaseDirectory() + "/" + getTrialId().toString());
+    }
+
     @Override
     public void create() {
-        File trialFolder = new File(getBaseDirectory() + "/" + getTrialId().toString());
-        if (trialFolder.mkdirs()) {
+        if (getTrialDirectory().mkdirs()) {
             log.info(LoggerColors.success("Trial " + getTrialId() + ": folder created"));
         } else {
             throw new FileCreationFailed("Trial " + getTrialId() + ": folder creation failed");
@@ -52,6 +55,21 @@ public class TrialStorageImpl implements TrialStorage {
     @Override
     public File get() {
         return new File(getBaseDirectory() + "/" + getTrialId().toString());
+    }
+
+    @Override
+    public void appendLog(AvogadorLogMessage avogadorLogMessage) throws IOException {
+        File logFile = new File(getTrialDirectory() + "/" + getTrialId() + ".avlog");
+        if (logFile.createNewFile()) {
+            log.info(LoggerColors.success("Trial " + getTrialId() + ": log file created"));
+        }
+        FileUtils.writeStringToFile(logFile, avogadorLogMessage.toString(), StandardCharsets.UTF_8, true);
+    }
+
+    @Override
+    public List<String> getLogs() throws IOException {
+        File logFile = new File(getTrialDirectory() + "/" + getTrialId() + ".avlog");
+        return Files.readAllLines(logFile.toPath());
     }
 
     @Override
