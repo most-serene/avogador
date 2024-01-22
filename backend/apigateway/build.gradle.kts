9def86653f32f96f9f835dc5e2a8b7fa24125c74
Mario Gottardo <mario.gottardo27@gmail.com>
2024-01-22 11:31:28 +0100

    Codebase adapted to version 0.7.0

--- backend/apigateway/build.gradle.kts
+++ backend/apigateway/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "eu.mostserene.avogador"
-version = "0.6.0"
+version = "0.7.0"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
--- backend/apigateway/src/main/java/eu/mostserene/avogador/apigateway/ApiGatewayApplication.java
+++ backend/apigateway/src/main/java/eu/mostserene/avogador/apigateway/ApiGatewayApplication.java
@@ -108,7 +108,7 @@ public class ApiGatewayApplication {
 
     @GetMapping("/version/webapp")
     String getWebappVersion() {
-        return "0.6.0";
+        return "0.7.0";
     }
 
     @PostConstruct
