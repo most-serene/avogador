963ad14138e2ad87c35def3d6837ec3897b58fc7
Mario Gottardo <mario.gottardo27@gmail.com>
2024-02-13 17:29:46 +0100

    AVG-384: course service archive method

--- backend/services/courseservice/src/main/java/eu/mostserene/avogador/courseservice/courses/CourseService.java
+++ backend/services/courseservice/src/main/java/eu/mostserene/avogador/courseservice/courses/CourseService.java
@@ -1,15 +1,25 @@
 package eu.mostserene.avogador.courseservice.courses;
 
+import eu.mostserene.avogador.courseservice.usercourses.UserCourse;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface CourseService {
     Course createCourse(Course course);
+
     Course updateCourse(UUID id, Course course);
+
     Optional<Course> getCourse(UUID id);
+
     List<Course> getAll();
+
     void deleteCourse(UUID courseId);
+
+    Course archiveCourse(UserCourse course);
+
     Optional<String> getJoinCode(UUID courseId);
+
     Optional<Course> getByNameAndYear(String name, String year);
 }
