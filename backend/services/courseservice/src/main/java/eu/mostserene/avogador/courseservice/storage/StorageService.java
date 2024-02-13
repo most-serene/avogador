a6f79d064e8471f5885ab1cbd10bc76378e75632
Mario Gottardo <mario.gottardo27@gmail.com>
2024-02-13 17:26:50 +0100

    AVG-384: course storage service archive method

--- backend/services/courseservice/src/main/java/eu/mostserene/avogador/courseservice/storage/StorageService.java
+++ backend/services/courseservice/src/main/java/eu/mostserene/avogador/courseservice/storage/StorageService.java
@@ -1,9 +1,12 @@
 package eu.mostserene.avogador.courseservice.storage;
 
 import java.util.UUID;
+import java.util.function.BiConsumer;
 
 public interface StorageService {
     void createCourse(UUID courseId);
+
     Integer deleteCourse(UUID courseId);
-    Integer archiveCourse(UUID courseId);
+
+    void archiveCourse(UUID courseId, BiConsumer<Boolean, Throwable> handler);
 }
--- backend/services/courseservice/src/main/java/eu/mostserene/avogador/courseservice/storage/StorageServiceImpl.java
+++ backend/services/courseservice/src/main/java/eu/mostserene/avogador/courseservice/storage/StorageServiceImpl.java
@@ -1,12 +1,15 @@
 package eu.mostserene.avogador.courseservice.storage;
 
 import eu.mostserene.avogador.courseservice.amqp.Sender;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
+import java.util.function.BiConsumer;
 
 @Service
+@Slf4j
 public class StorageServiceImpl implements StorageService {
 
     @Autowired
@@ -23,7 +26,7 @@ public class StorageServiceImpl implements StorageService {
     }
 
     @Override
-    public Integer archiveCourse(UUID courseId) {
-        throw new UnsupportedOperationException("Method not yet implemented");
+    public void archiveCourse(UUID courseId, BiConsumer<Boolean, Throwable> handler) {
+        sender.send("storage", "storage.course.archive", courseId.toString(), handler);
     }
 }
