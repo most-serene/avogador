af408dc8c0422e6a623ab1a25bcbd7bafb6b1bbe
Alessio Campanelli <64778464+Alessio-Campa@users.noreply.github.com>
2024-02-21 17:21:04 +0100

    AVG-394 user-service ProfileManager refactor (#323)
    * AVG-395 user-service ProfileManager refactor
    
    * AVG-394 Better profile management

---
+++ backend/services/userservice/src/main/java/eu/mostserene/avogador/userservice/profilemanager/DevelopExecutionProfile.java
@@ -0,0 +1,50 @@
+package eu.mostserene.avogador.userservice.profilemanager;
+
+import eu.mostserene.avogador.userservice.users.User;
+import lombok.Getter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Getter
+@Service
+public class DevelopExecutionProfile implements ExecutionProfile {
+    private final String JWTKey = "develop-jwt";
+
+    public ResponseCookie buildJWT(User user, String value) {
+        return ResponseCookie.from(JWTKey)
+                .value(value)
+                .httpOnly(true)
+                .path("/")
+                .domain("localhost")
+                .secure(true)
+                .maxAge(Duration.ofDays(7))
+                .sameSite("None")
+                .build();
+    }
+
+    public ResponseCookie logout() {
+        return ResponseCookie.from(JWTKey)
+                .value(null)
+                .httpOnly(true)
+                .path("/")
+                .domain("localhost")
+                .secure(true)
+                .maxAge(Duration.ofSeconds(1))
+                .sameSite("None")
+                .build();
+    }
+}
+
+@Configuration
+@Profile("develop")
+class DevelopExecutionProfileConfiguration {
+    @Bean
+    public ExecutionProfile executionProfile(){
+        return new DevelopExecutionProfile();
+    }
+}
