50dfd2a44cf032fd48101914678acc925b2ace00
Mario Gottardo <mario.gottardo27@gmail.com>
2024-03-11 14:55:53 +0100

    AVG-410: executor dockerfile refactor

--- backend/services/executorservice/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-
-curl -k https://repository.mostserene.eu/avogador/artifacts/executorservice.jar -u ${REPOSITORY_CREDENTIALS} --output executorservice.jar
-java -jar ./executorservice.jar
