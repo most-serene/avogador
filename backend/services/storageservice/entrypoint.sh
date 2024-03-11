b0e0764cd0ddda2a1fa3fa5311dcd1cdca80337d
Mario Gottardo <mario.gottardo27@gmail.com>
2024-03-11 14:57:19 +0100

    AVG-410: storage dockerfile refactor

--- backend/services/storageservice/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-
-curl -k https://repository.mostserene.eu/avogador/artifacts/storageservice.jar -u ${REPOSITORY_CREDENTIALS}  --output storageservice.jar
-java -jar ./storageservice.jar
