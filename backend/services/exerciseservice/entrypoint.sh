b6bc19b44c62a51204263b733a41b4d4450933af
Mario Gottardo <mario.gottardo27@gmail.com>
2024-03-11 14:56:25 +0100

    AVG-410: exercise dockerfile refactor

--- backend/services/exerciseservice/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-
-curl -k https://repository.mostserene.eu/avogador/artifacts/exerciseservice.jar -u ${REPOSITORY_CREDENTIALS} --output exerciseservice.jar
-java -jar ./exerciseservice.jar
