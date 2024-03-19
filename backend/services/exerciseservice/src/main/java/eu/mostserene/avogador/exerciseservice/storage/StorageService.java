161ef342d175f7da2b10a56c29a4684ef2b4b735
Mario Gottardo <mario.gottardo27@gmail.com>
2024-03-19 14:15:37 +0100

    AVG-415: storage service adapted

--- backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/storage/StorageService.java
+++ backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/storage/StorageService.java
@@ -1,7 +1,7 @@
 package eu.mostserene.avogador.exerciseservice.storage;
 
+import eu.mostserene.avogador.exerciseservice.abstractexercises.codingexercises.CodingExercise;
 import eu.mostserene.avogador.exerciseservice.antiplagiarism.PlagiarismReport;
-import eu.mostserene.avogador.exerciseservice.exercises.Exercise;
 import eu.mostserene.avogador.exerciseservice.strox.Strox;
 import eu.mostserene.avogador.exerciseservice.submissions.Submission;
 import eu.mostserene.avogador.exerciseservice.testcases.TestcaseDetailDto;
@@ -17,20 +17,36 @@ import java.util.UUID;
 
 public interface StorageService {
     void createTrial(Trial trial);
+
     void deleteTrial(Trial trial);
-    void createExercise(Exercise exercise);
-    void createExerciseTemplate(Exercise exercise, Strox template);
-    void deleteExercise(Exercise exercise);
-    void createTestcase(Exercise exercise, TestcaseDetailDto testcase);
-    void deleteTestcase(Exercise exercise, UUID testcaseId);
-    Optional<TestcaseIODto> getTestcase(Exercise exercise, UUID testcaseId);
-    void updateTestcase(Exercise exercise,TestcaseDetailDto testcase);
+
+    void createExercise(CodingExercise exercise);
+
+    void createExerciseTemplate(CodingExercise exercise, Strox template);
+
+    void deleteExercise(CodingExercise exercise);
+
+    void createTestcase(CodingExercise exercise, TestcaseDetailDto testcase);
+
+    void deleteTestcase(CodingExercise exercise, UUID testcaseId);
+
+    Optional<TestcaseIODto> getTestcase(CodingExercise exercise, UUID testcaseId);
+
+    void updateTestcase(CodingExercise exercise, TestcaseDetailDto testcase);
+
     void createSubmission(Submission submission, Strox strox);
+
     Optional<Strox> getSubmissionStrox(Submission submission);
+
     Optional<Resource> getSubmissionSource(Submission submission);
-    Optional<Strox> getExerciseTemplate(Exercise exercise);
+
+    Optional<Strox> getExerciseTemplate(CodingExercise exercise);
+
     Optional<Strox> getMergedSubmission(Submission submission);
-    Resource getExerciseLatestSubmissionsSources(Exercise exercise, List<UUID> submissionIds);
-    void uploadSimilarityReport(Exercise exercise, File reportZip);
-    Optional<PlagiarismReport> getSimilarityReport(Exercise exercise);
+
+    Resource getExerciseLatestSubmissionsSources(CodingExercise exercise, List<UUID> submissionIds);
+
+    void uploadSimilarityReport(CodingExercise exercise, File reportZip);
+
+    Optional<PlagiarismReport> getSimilarityReport(CodingExercise exercise);
 }
