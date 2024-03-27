915b6aa879dca7495b3d3f9c89d73b09a84e4fab
Mario Gottardo <mario.gottardo27@gmail.com>
2024-03-27 17:48:10 +0100

    AVG-420: better Submissions type structure

--- backend/services/executorservice/src/main/java/eu/mostserene/avogador/executorservice/submission/CodingSubmission.java
+++ backend/services/executorservice/src/main/java/eu/mostserene/avogador/executorservice/submission/CodingSubmission.java
@@ -1,14 +1,15 @@
 package eu.mostserene.avogador.executorservice.submission;
 
+import eu.mostserene.avogador.executorservice.executor.Submission;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.List;
 import java.util.UUID;
 
 @Data
-public class CodingSubmission {
-    private UUID id;
-    private UUID courseId;
+@EqualsAndHashCode(callSuper = true)
+public class CodingSubmission extends Submission {
     private UUID trialId;
     private UUID exerciseId;
     private String language;
