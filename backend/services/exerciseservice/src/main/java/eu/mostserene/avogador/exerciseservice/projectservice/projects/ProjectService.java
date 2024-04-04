9b0e936629360eb5fbc2ee31dea895cd5d634c5d
Mario Gottardo <mario.gottardo27@gmail.com>
2024-04-04 23:22:37 +0200

    AVG-426: getProjectById method

--- backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/projectservice/projects/ProjectService.java
+++ backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/projectservice/projects/ProjectService.java
@@ -1,4 +1,8 @@
 package eu.mostserene.avogador.exerciseservice.projectservice.projects;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ProjectService {
+    Optional<Project> getProjectById(UUID projectId);
 }
--- backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/projectservice/projects/ProjectServiceImpl.java
+++ backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/projectservice/projects/ProjectServiceImpl.java
@@ -1,9 +1,20 @@
 package eu.mostserene.avogador.exerciseservice.projectservice.projects;
 
 import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Transactional
 @Service
 public class ProjectServiceImpl implements ProjectService {
+    @Autowired
+    private ProjectRepository repository;
+
+    @Override
+    public Optional<Project> getProjectById(UUID projectId) {
+        return repository.findById(projectId);
+    }
 }
