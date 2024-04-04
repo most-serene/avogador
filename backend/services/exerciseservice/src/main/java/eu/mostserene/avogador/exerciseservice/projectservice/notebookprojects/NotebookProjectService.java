23293382194b81facc600fc73a5177bbd7c3a212
Mario Gottardo <mario.gottardo27@gmail.com>
2024-04-04 23:21:27 +0200

    AVG-426: getNotebookProject method

--- backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/projectservice/notebookprojects/NotebookProjectService.java
+++ backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/projectservice/notebookprojects/NotebookProjectService.java
@@ -1,5 +1,9 @@
 package eu.mostserene.avogador.exerciseservice.projectservice.notebookprojects;
 
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface NotebookProjectService {
+    Optional<NotebookProject> getNotebookProject(UUID projectId);
 }
