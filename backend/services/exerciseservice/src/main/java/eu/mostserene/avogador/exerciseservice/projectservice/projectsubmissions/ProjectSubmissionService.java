7252862f2dbf5e4b5ab124cc6882e6d06b4dbb35
Mario Gottardo <mario.gottardo27@gmail.com>
2024-04-04 23:32:28 +0200

    AVG-426: createSubmission method

--- backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/projectservice/projectsubmissions/ProjectSubmissionService.java
+++ backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/projectservice/projectsubmissions/ProjectSubmissionService.java
@@ -1,9 +1,15 @@
 package eu.mostserene.avogador.exerciseservice.projectservice.projectsubmissions;
 
+import eu.mostserene.avogador.exerciseservice.projectservice.projects.Project;
+import eu.mostserene.avogador.exerciseservice.users.UserDto;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ProjectSubmissionService {
+    ProjectSubmission createSubmission(Project project, UserDto user, MultipartFile file);
+
     Optional<ProjectSubmission> getProjectSubmissionById(UUID submissionId);
 
     ProjectSubmission setProjectSubmissionStatus(UUID submissionStatus, ProjectStatus status);
