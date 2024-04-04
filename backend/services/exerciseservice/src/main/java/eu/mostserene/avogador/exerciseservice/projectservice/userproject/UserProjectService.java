6046cfa972ddf89132255960458eece0920508c7
Mario Gottardo <mario.gottardo27@gmail.com>
2024-04-04 23:23:14 +0200

    AVG-426: getUserProject method

--- backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/projectservice/userproject/UserProjectService.java
+++ backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/projectservice/userproject/UserProjectService.java
@@ -1,4 +1,9 @@
 package eu.mostserene.avogador.exerciseservice.projectservice.userproject;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface UserProjectService {
+
+    Optional<UserProject> getUserProject(UUID projectId, UUID userId);
 }
--- backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/projectservice/userproject/UserProjectServiceImpl.java
+++ backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/projectservice/userproject/UserProjectServiceImpl.java
@@ -2,9 +2,21 @@ package eu.mostserene.avogador.exerciseservice.projectservice.userproject;
 
 
 import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Transactional
 @Service
 public class UserProjectServiceImpl implements UserProjectService {
+
+    @Autowired
+    private UserProjectRepository repository;
+
+    @Override
+    public Optional<UserProject> getUserProject(UUID projectId, UUID userId) {
+        return repository.findByProject_IdAndUserId(projectId, userId);
+    }
 }
