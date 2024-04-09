369c7ad9cac5afa4b102f6fd379e1f440636dca9
Mario Gottardo <mario.gottardo27@gmail.com>
2024-04-09 08:37:59 +0200

    AVG-430: createProject and updateProject notebook methods

--- backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/projectservice/notebookprojects/NotebookProjectServiceImpl.java
+++ backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/projectservice/notebookprojects/NotebookProjectServiceImpl.java
@@ -1,5 +1,6 @@
 package eu.mostserene.avogador.exerciseservice.projectservice.notebookprojects;
 
+import eu.mostserene.avogador.exerciseservice.storage.StorageService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +15,29 @@ public class NotebookProjectServiceImpl implements NotebookProjectService {
     @Autowired
     private NotebookProjectRepository repository;
 
+    @Autowired
+    private StorageService storageService;
+
     @Override
     public Optional<NotebookProject> getNotebookProject(UUID projectId) {
         return repository.findById(projectId);
+    }
+
+    @Override
+    public NotebookProject createProject(NotebookProject project) {
+        NotebookProject savedProject = repository.save(new NotebookProject(
+                project.getCourseId(),
+                project.getName(),
+                project.getDescription(),
+                project.getCanSubmit(),
+                project.getDeadline()
+        ));
+        storageService.createProject(project);
+        return savedProject;
+    }
+
+    @Override
+    public NotebookProject updateProject(NotebookProject project) {
+        return repository.save(project);
     }
 }
