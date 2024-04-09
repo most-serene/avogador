00ab71dd97aab1ab911a6f0a0c16616be8e7a02b
Mario Gottardo <mario.gottardo27@gmail.com>
2024-04-09 21:23:26 +0200

    AVG-430: getProjectSubmission endpoint

--- backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/projectservice/projectsubmissions/ProjectSubmissionController.java
+++ backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/projectservice/projectsubmissions/ProjectSubmissionController.java
@@ -1,7 +1,49 @@
 package eu.mostserene.avogador.exerciseservice.projectservice.projectsubmissions;
 
-import org.springframework.web.bind.annotation.RestController;
+import eu.mostserene.avogador.exerciseservice.courses.CourseDetailDto;
+import eu.mostserene.avogador.exerciseservice.courses.UserCourseService;
+import eu.mostserene.avogador.exerciseservice.projectservice.projects.Project;
+import eu.mostserene.avogador.exerciseservice.projectservice.projects.ProjectService;
+import eu.mostserene.avogador.exerciseservice.security.ForbiddenException;
+import eu.mostserene.avogador.exerciseservice.users.UserDto;
+import eu.mostserene.avogador.exerciseservice.utils.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/public/projects/{projectId}/submissions")
 public class ProjectSubmissionController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @Autowired
+    private ProjectSubmissionService projectSubmissionService;
+
+    @Autowired
+    private UserCourseService userCourseService;
+
+    @GetMapping("/{submissionId}")
+    private ProjectSubmission getProjectSubmission(@RequestHeader(name = "User") UserDto user,
+                                                   @PathVariable UUID projectId,
+                                                   @PathVariable UUID submissionId) {
+        Project project = projectService.getProjectById(projectId)
+                .orElseThrow(NotFoundException::new);
+
+        CourseDetailDto course = userCourseService.getUserCourseRoleDetail(project.getCourseId(), user.getId())
+                .orElseThrow(NotFoundException::new);
+
+        ProjectSubmission submission = projectSubmissionService.getProjectSubmissionById(submissionId)
+                .orElseThrow(NotFoundException::new);
+
+        if (!user.getId().equals(submission.getUserId()) &&
+                !course.getRole().hasCollaboratorClearance() &&
+                !user.getIsSuperuser()) {
+            throw new ForbiddenException(user, "You cannot see this submission");
+        }
+
+        return submission;
+    }
 }
