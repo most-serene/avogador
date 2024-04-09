be1ed26b2d6a90026261254bfa2c3712d2a9acd6
Mario Gottardo <mario.gottardo27@gmail.com>
2024-04-09 09:33:17 +0200

    AVG-430: joinProject method

--- backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/projectservice/userproject/UserProjectService.java
+++ backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/projectservice/userproject/UserProjectService.java
@@ -1,9 +1,14 @@
 package eu.mostserene.avogador.exerciseservice.projectservice.userproject;
 
+import eu.mostserene.avogador.exerciseservice.projectservice.projects.Project;
+import eu.mostserene.avogador.exerciseservice.users.UserDto;
+
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserProjectService {
 
     Optional<UserProject> getUserProject(UUID projectId, UUID userId);
+
+    UserProject joinProject(UserDto user, Project project);
 }
--- backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/projectservice/userproject/UserProjectServiceImpl.java
+++ backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/projectservice/userproject/UserProjectServiceImpl.java
@@ -1,6 +1,8 @@
 package eu.mostserene.avogador.exerciseservice.projectservice.userproject;
 
 
+import eu.mostserene.avogador.exerciseservice.projectservice.projects.Project;
+import eu.mostserene.avogador.exerciseservice.users.UserDto;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,5 +20,10 @@ public class UserProjectServiceImpl implements UserProjectService {
     @Override
     public Optional<UserProject> getUserProject(UUID projectId, UUID userId) {
         return repository.findByProject_IdAndUserId(projectId, userId);
+    }
+
+    @Override
+    public UserProject joinProject(UserDto user, Project project) {
+        return repository.save(new UserProject(user.getId(), project));
     }
 }
