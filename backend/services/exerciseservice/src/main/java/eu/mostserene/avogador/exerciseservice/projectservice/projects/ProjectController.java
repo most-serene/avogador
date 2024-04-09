f3993cc4fa0aa3a8d7aee5efcd31d80ff01fe7e7
Mario Gottardo <mario.gottardo27@gmail.com>
2024-04-09 09:34:57 +0200

    AVG-430: joinProject endpoint

--- backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/projectservice/projects/ProjectController.java
+++ backend/services/exerciseservice/src/main/java/eu/mostserene/avogador/exerciseservice/projectservice/projects/ProjectController.java
@@ -3,6 +3,8 @@ package eu.mostserene.avogador.exerciseservice.projectservice.projects;
 import eu.mostserene.avogador.exerciseservice.courses.CourseDetailDto;
 import eu.mostserene.avogador.exerciseservice.courses.CourseRole;
 import eu.mostserene.avogador.exerciseservice.courses.UserCourseService;
+import eu.mostserene.avogador.exerciseservice.projectservice.userproject.UserProject;
+import eu.mostserene.avogador.exerciseservice.projectservice.userproject.UserProjectService;
 import eu.mostserene.avogador.exerciseservice.security.ForbiddenException;
 import eu.mostserene.avogador.exerciseservice.users.UserDto;
 import eu.mostserene.avogador.exerciseservice.utils.NotFoundException;
@@ -20,6 +22,9 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private UserProjectService userProjectService;
+
     @GetMapping("/{projectId}")
     private Project getProjectById(@RequestHeader(name = "User") UserDto user,
                                    @PathVariable UUID projectId) {
@@ -34,5 +39,25 @@ public class ProjectController {
         }
 
         return project;
+    }
+
+    @PutMapping("/{projectId}/join")
+    private UserProject joinProject(@RequestHeader(name = "User") UserDto user,
+                                    @PathVariable UUID projectId) {
+        Project project = projectService.getProjectById(projectId)
+                .orElseThrow(NotFoundException::new);
+
+        CourseDetailDto course = userCourseService.getUserCourseRoleDetail(project.getCourseId(), user.getId())
+                .orElseThrow(NotFoundException::new)
+                .requireNotArchived();
+
+        if (course.getRole().equals(CourseRole.EXTERNAL) && !user.getIsSuperuser()) {
+            throw new ForbiddenException(user, "You are not a course member");
+        }
+
+        if (user.getIsSuperuser() || course.getRole().getClearance() >= CourseRole.COLLABORATOR.getClearance()) {
+            return null;
+        }
+        return userProjectService.joinProject(user, project);
     }
 }
