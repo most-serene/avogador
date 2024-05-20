706c2ed30a4199217391fac45a97d61d2d9f6de4
Mario Gottardo <58598676+Gotti27@users.noreply.github.com>
2024-05-20 14:45:12 +0200

    Codebase version adapted to 0.14.0 (#394)

--- backend/apigateway/src/main/java/eu/mostserene/avogador/apigateway/ApiGatewayApplication.java
+++ backend/apigateway/src/main/java/eu/mostserene/avogador/apigateway/ApiGatewayApplication.java
@@ -121,7 +121,7 @@ public class ApiGatewayApplication {
 
     @GetMapping("/version/webapp")
     String getWebappVersion() {
-        return "0.13.0";
+        return "0.14.0";
     }
 
     @PostConstruct
