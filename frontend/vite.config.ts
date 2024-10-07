8adef1d575ea517ac2f1044d8ab696e088ee4b59
Mario Gottardo <mario.gottardo27@gmail.com>
2024-10-07 09:49:30 +0200

    added sourcemaps generation to vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -110,6 +110,7 @@ export default defineConfig({
   },
   build: {
     chunkSizeWarningLimit: 1000,
+    sourcemap: 'hidden'
   },
   optimizeDeps: {
     include: ["@emotion/react", "@emotion/styled"],
