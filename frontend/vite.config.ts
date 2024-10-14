5655f7dadcdb8e4387f0c9ae7ed63e17e0e1d0be
Mario Gottardo <58598676+Gotti27@users.noreply.github.com>
2024-10-14 15:05:44 +0200

    AVG-458: obfuscator removal and sourcemap upload (#411)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite";
+import {sentryVitePlugin} from "@sentry/vite-plugin";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { VitePWA } from "vite-plugin-pwa";
-import { fileURLToPath, URL } from "url";
+import {VitePWA} from "vite-plugin-pwa";
+import {fileURLToPath, URL} from "url";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
+export default defineConfig(({mode}) => {
+  return {
+    plugins: [react({
       jsxImportSource: "@emotion/react",
-    }),
-    VitePWA({
+    }), VitePWA({
       registerType: "autoUpdate",
       // includeAssets: ['MostSerene.svg', 'avogador.png', '512.png'],
       manifest: {
@@ -31,87 +31,105 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
-    }),
-  ],
-  resolve: {
-    alias: {
-      //"@": fileURLToPath(new URL("./", import.meta.url)),
-      "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
-      "@assets/*": fileURLToPath(new URL("./src/assets/*", import.meta.url)),
-      "@components": fileURLToPath(
-        new URL("./src/components", import.meta.url),
-      ),
-      "@components/*": fileURLToPath(
-        new URL("./src/components/*", import.meta.url),
-      ),
-      "@authentication": fileURLToPath(
-        new URL("./src/components/authentication", import.meta.url),
-      ),
-      "@authentication/*": fileURLToPath(
-        new URL("./src/components/authentication/*", import.meta.url),
-      ),
-      "@courses": fileURLToPath(
-        new URL("./src/components/courses", import.meta.url),
-      ),
-      "@courses/*": fileURLToPath(
-        new URL("./src/components/courses/*", import.meta.url),
-      ),
-      "@exercises": fileURLToPath(
-        new URL("./src/components/exercises", import.meta.url),
-      ),
-      "@exercises/*": fileURLToPath(
-        new URL("./src/components/exercises/*", import.meta.url),
-      ),
-      "@error": fileURLToPath(
-        new URL("./src/components/error", import.meta.url),
-      ),
-      "@error/*": fileURLToPath(
-        new URL("./src/components/error/*", import.meta.url),
-      ),
-      "@home": fileURLToPath(new URL("./src/components/home", import.meta.url)),
-      "@home/*": fileURLToPath(
-        new URL("./src/components/home/*", import.meta.url),
-      ),
-      "@profile": fileURLToPath(
-        new URL("./src/components/profile", import.meta.url),
-      ),
-      "@profile/*": fileURLToPath(
-        new URL("./src/components/profile/*", import.meta.url),
-      ),
-      "@structure": fileURLToPath(
-        new URL("./src/components/structure", import.meta.url),
-      ),
-      "@structure/*": fileURLToPath(
-        new URL("./src/components/structure/*", import.meta.url),
-      ),
-      "@theme": fileURLToPath(
-        new URL("./src/components/theme", import.meta.url),
-      ),
-      "@theme/*": fileURLToPath(
-        new URL("./src/components/theme/*", import.meta.url),
-      ),
-      "@trials": fileURLToPath(
-        new URL("./src/components/trials", import.meta.url),
-      ),
-      "@trials/*": fileURLToPath(
-        new URL("./src/components/trials/*", import.meta.url),
-      ),
-      "@hooks": fileURLToPath(new URL("./src/hooks", import.meta.url)),
-      "@hooks/*": fileURLToPath(new URL("./src/hooks/*", import.meta.url)),
+    }), sentryVitePlugin({
+      org: "mostserene",
+      project: "avogador-frontend",
+      url: "https://app.glitchtip.com",
+      disable: mode !== 'production',
+      release: {
+        name: process.env.npm_package_version,
+        dist: process.env.npm_package_version,
+        inject: true,
+        create: true,
+        finalize: false
+      },
+      sourcemaps: {
+        filesToDeleteAfterUpload: 'dist/**.js.map',
+        // assets: 'dist/**/*.js.map'
+      },
+      debug: true,
+    })],
+    resolve: {
+      alias: {
+        //"@": fileURLToPath(new URL("./", import.meta.url)),
+        "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+        "@assets/*": fileURLToPath(new URL("./src/assets/*", import.meta.url)),
+        "@components": fileURLToPath(
+          new URL("./src/components", import.meta.url),
+        ),
+        "@components/*": fileURLToPath(
+          new URL("./src/components/*", import.meta.url),
+        ),
+        "@authentication": fileURLToPath(
+          new URL("./src/components/authentication", import.meta.url),
+        ),
+        "@authentication/*": fileURLToPath(
+          new URL("./src/components/authentication/*", import.meta.url),
+        ),
+        "@courses": fileURLToPath(
+          new URL("./src/components/courses", import.meta.url),
+        ),
+        "@courses/*": fileURLToPath(
+          new URL("./src/components/courses/*", import.meta.url),
+        ),
+        "@exercises": fileURLToPath(
+          new URL("./src/components/exercises", import.meta.url),
+        ),
+        "@exercises/*": fileURLToPath(
+          new URL("./src/components/exercises/*", import.meta.url),
+        ),
+        "@error": fileURLToPath(
+          new URL("./src/components/error", import.meta.url),
+        ),
+        "@error/*": fileURLToPath(
+          new URL("./src/components/error/*", import.meta.url),
+        ),
+        "@home": fileURLToPath(new URL("./src/components/home", import.meta.url)),
+        "@home/*": fileURLToPath(
+          new URL("./src/components/home/*", import.meta.url),
+        ),
+        "@profile": fileURLToPath(
+          new URL("./src/components/profile", import.meta.url),
+        ),
+        "@profile/*": fileURLToPath(
+          new URL("./src/components/profile/*", import.meta.url),
+        ),
+        "@structure": fileURLToPath(
+          new URL("./src/components/structure", import.meta.url),
+        ),
+        "@structure/*": fileURLToPath(
+          new URL("./src/components/structure/*", import.meta.url),
+        ),
+        "@theme": fileURLToPath(
+          new URL("./src/components/theme", import.meta.url),
+        ),
+        "@theme/*": fileURLToPath(
+          new URL("./src/components/theme/*", import.meta.url),
+        ),
+        "@trials": fileURLToPath(
+          new URL("./src/components/trials", import.meta.url),
+        ),
+        "@trials/*": fileURLToPath(
+          new URL("./src/components/trials/*", import.meta.url),
+        ),
+        "@hooks": fileURLToPath(new URL("./src/hooks", import.meta.url)),
+        "@hooks/*": fileURLToPath(new URL("./src/hooks/*", import.meta.url)),
+      },
     },
-  },
-  define: {
-    "import.meta.env.APP_VERSION": JSON.stringify(
-      process.env.npm_package_version,
-    ),
-  },
-  server: {
-    port: 3000,
-  },
-  build: {
-    chunkSizeWarningLimit: 1000,
-  },
-  optimizeDeps: {
-    include: ["@emotion/react", "@emotion/styled"],
-  },
+    define: {
+      "import.meta.env.APP_VERSION": JSON.stringify(
+        process.env.npm_package_version,
+      ),
+    },
+    server: {
+      port: 3000,
+    },
+    build: {
+      chunkSizeWarningLimit: 1000,
+      sourcemap: true,
+    },
+    optimizeDeps: {
+      include: ["@emotion/react", "@emotion/styled"],
+    },
+  }
 });
