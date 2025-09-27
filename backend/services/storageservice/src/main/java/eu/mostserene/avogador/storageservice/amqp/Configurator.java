7523df23eedc277c8ad17a707dc4df20347ad420
Mario Gottardo <mario.gottardo27@gmail.com>
2025-09-27 13:21:53 +0200

    Fixed RabbitMQ reconnection error

--- backend/services/storageservice/src/main/java/eu/mostserene/avogador/storageservice/amqp/Configurator.java
+++ backend/services/storageservice/src/main/java/eu/mostserene/avogador/storageservice/amqp/Configurator.java
@@ -44,7 +44,8 @@ public class Configurator {
 
     @Bean
     public ConnectionFactory connectionFactory() {
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(rabbitHostname);
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost(rabbitHostname);
         connectionFactory.setUsername(rabbitUsername);
         connectionFactory.setPassword(rabbitPassword);
         return connectionFactory;
